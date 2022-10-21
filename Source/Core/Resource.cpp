ea60cdd0427ead14da884dd4edc032c8371e534a
bad-wolf-jm <jm.albert@gmail.com>
2022-10-21 11:58:50 -0400

    Bug fixes

--- Source/Core/Resource.cpp
+++ Source/Core/Resource.cpp
@@ -4,7 +4,7 @@ namespace LTSE::Core
 {
 
     // static fs::path a_ResourceRoot = "Deps\\LTSimulationEngine\\Resources";
-    static fs::path a_ResourceRoot = "C:\\GitLab\\Echods_FLM\\Deps\\LTSimulationEngine\\Resources";
+    static fs::path a_ResourceRoot = "C:\\GitLab\\SpockEngine\\Resources";
 
     fs::path GetResourcePath( fs::path a_RelativePath ) { return a_ResourceRoot / a_RelativePath; }
 
