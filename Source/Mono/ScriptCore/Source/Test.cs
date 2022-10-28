fc0740baa36d8440cd3102e1ffa910e9b64a8afe
bad-wolf-jm <jm.albert@gmail.com>
2022-10-28 12:00:52 -0400

    Added test program

---
+++ Source/Mono/ScriptCore/Source/Test.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SE
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
