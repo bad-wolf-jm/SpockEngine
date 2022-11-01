50deaee21c912ac73fca06f7f2f946f4705b0c3d
bad-wolf-jm <jm.albert@gmail.com>
2022-11-01 14:07:42 -0400

    Script version of sNodeTransformComponent

--- Source/ScriptCore/Source/Component.cs
+++ Source/ScriptCore/Source/Component.cs
@@ -16,16 +16,9 @@ namespace SpockEngine
             Console.WriteLine($"New sNodeTransformComponent instantiated!!");
         }
 
-        public sNodeTransformComponent( ref mat4 aMatrix ) 
+        public sNodeTransformComponent( mat4 aMatrix ) 
         { 
             mMatrix = aMatrix;
-
-            Console.WriteLine($"New sNodeTransformComponent instantiated with matrix!!");
-            Console.WriteLine($"{mMatrix.m00} {mMatrix.m01} {mMatrix.m02} {mMatrix.m03}");
-            Console.WriteLine($"{mMatrix.m10} {mMatrix.m11} {mMatrix.m12} {mMatrix.m13}");
-            Console.WriteLine($"{mMatrix.m20} {mMatrix.m21} {mMatrix.m22} {mMatrix.m23}");
-            Console.WriteLine($"{mMatrix.m30} {mMatrix.m31} {mMatrix.m32} {mMatrix.m33}");
-
         }
 
     }
