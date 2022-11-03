629c5ee622a1894a3c3af1366418c57b30156d1d
bad-wolf-jm <jm.albert@gmail.com>
2022-11-03 09:23:36 -0400

    Reverts changes

--- Source/Core/Platform/Window.cpp
+++ Source/Core/Platform/Window.cpp
@@ -117,7 +117,7 @@ namespace LTSE::Core
         lUserEvent.Modifiers     = ModifierFlags( (uint8_t)( (int32_t)aModifiers & 0xff ) );
 
         lWindow->mEngineLoop->IOEvent( lUserEvent );
-        if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
+        // if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
     }
 
     void Window::OnMouseButton( GLFWwindow *aWindow, const int button, const int aAction, const int aModifiers )
@@ -142,7 +142,7 @@ namespace LTSE::Core
         lUserEvent.KeyCode       = Key::Unknown;
         lUserEvent.Modifiers     = ModifierFlags( (uint8_t)( (int32_t)aModifiers & 0xff ) );
         lWindow->mEngineLoop->IOEvent( lUserEvent );
-        if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
+        // if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
     }
 
     void Window::OnCursorPosition( GLFWwindow *aWindow, const double x, const double y )
@@ -163,7 +163,7 @@ namespace LTSE::Core
 
         lWindow->mLastMousePosition = l_CurrentMousePosition;
         lWindow->mEngineLoop->IOEvent( lUserEvent );
-        if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
+        // if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
     }
 
     void Window::OnMouseScroll( GLFWwindow *aWindow, const double dx, const double dy )
@@ -183,7 +183,7 @@ namespace LTSE::Core
         lWindow->mLastMousePosition = math::vec2{ static_cast<int32_t>( xpos ), static_cast<int32_t>( ypos ) };
 
         lWindow->mEngineLoop->IOEvent( lUserEvent );
-        if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
+        // if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
     }
 
     void Window::OnTextInput( GLFWwindow *aWindow, unsigned int codepoint )
@@ -196,7 +196,7 @@ namespace LTSE::Core
         lUserEvent.KeyCode       = Key::Unknown;
         lUserEvent.Modifiers     = ModifierFlags();
         lWindow->mEngineLoop->IOEvent( lUserEvent );
-        if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
+        // if( lWindow->IOEventDelegate ) lWindow->IOEventDelegate( lUserEvent );
     }
 
 } // namespace LTSE::Core
