dce414a8596b46b95a7a7a3db24c6d760d2b4df5
bad-wolf-jm <jm.albert@gmail.com>
2022-11-03 15:13:06 -0400

    Load the actual application

--- Source/Editor/BaseEditorApplication.cpp
+++ Source/Editor/BaseEditorApplication.cpp
@@ -16,7 +16,6 @@
 #include "Core/Cuda/ExternalMemory.h"
 #include "Core/Cuda/MultiTensor.h"
 
-
 #include "Scene/Components.h"
 #include "Scene/Importer/glTFImporter.h"
 
@@ -29,59 +28,15 @@ namespace LTSE::Editor
     using namespace LTSE::Cuda;
     using namespace LTSE::Core::EntityComponentSystem::Components;
 
-    BaseEditorApplication::BaseEditorApplication() { mEngineLoop = New<EngineLoop>(); }
-
-    void BaseEditorApplication::LoadConfiguration()
-    {
-        YAML::Node  l_RootNode  = YAML::LoadFile( ConfigurationFile.string() );
-        YAML::Node &l_ImGuiInit = l_RootNode["application"]["imgui_initialization"];
-        // if( !l_ImGuiInit.IsNull() ) ImGuiIniFile = l_ImGuiInit.as<std::string>();
-
-        YAML::Node &l_WindowProperties = l_RootNode["application"]["window_properties"];
-        if( !l_WindowProperties.IsNull() )
-        {
-            WindowPosition = { l_WindowProperties["x"].as<int>(), l_WindowProperties["y"].as<int>() };
-            WindowSize     = { l_WindowProperties["width"].as<int>(), l_WindowProperties["height"].as<int>() };
-        }
-    }
-
-    void BaseEditorApplication::SaveConfiguration()
-    {
-        YAML::Emitter out;
-        out << YAML::BeginMap;
-        {
-            out << YAML::Key << "application" << YAML::Value;
-            out << YAML::BeginMap;
-            {
-                // if( ImGuiIniFile.empty() )
-                    // out << YAML::Key << "imgui_initialization" << YAML::Value << YAML::Null;
-                // else
-                    // out << YAML::Key << "imgui_initialization" << YAML::Value << ImGuiIniFile;
-
-                out << YAML::Key << "window_properties" << YAML::Value << YAML::Flow;
-                {
-                    out << YAML::BeginMap;
-                    out << YAML::Key << "width" << YAML::Value << WindowSize.x;
-                    out << YAML::Key << "height" << YAML::Value << WindowSize.y;
-                    out << YAML::Key << "x" << YAML::Value << WindowPosition.x;
-                    out << YAML::Key << "y" << YAML::Value << WindowPosition.y;
-                    out << YAML::EndMap;
-                }
-            }
-            out << YAML::EndMap;
-        }
-        out << YAML::EndMap;
-        std::ofstream fout( ConfigurationFile );
-        fout << out.c_str();
-    }
+    BaseEditorApplication::BaseEditorApplication( Ref<EngineLoop> aEngineLoop ) { mEngineLoop = aEngineLoop; }
 
     void BaseEditorApplication::RenderScene()
     {
-        m_ViewportRenderContext.BeginRender();
+        mViewportRenderContext.BeginRender();
 
-        if( m_ViewportRenderContext ) m_WorldRenderer->Render( m_ViewportRenderContext );
+        if( mViewportRenderContext ) mWorldRenderer->Render( mViewportRenderContext );
 
-        m_ViewportRenderContext.EndRender();
+        mViewportRenderContext.EndRender();
     }
 
     void BaseEditorApplication::Update( Timestep ts )
@@ -92,66 +47,66 @@ namespace LTSE::Editor
 
     void BaseEditorApplication::RebuildOutputFramebuffer()
     {
-        if( m_ViewportWidth == 0 || m_ViewportHeight == 0 ) return;
+        if( mViewportWidth == 0 || mViewportHeight == 0 ) return;
 
-        if( !m_OffscreenRenderTarget )
+        if( !mOffscreenRenderTarget )
         {
             OffscreenRenderTargetDescription l_RenderTargetCI{};
-            l_RenderTargetCI.OutputSize  = { m_ViewportWidth, m_ViewportHeight };
+            l_RenderTargetCI.OutputSize  = { mViewportWidth, mViewportHeight };
             l_RenderTargetCI.SampleCount = 4;
             l_RenderTargetCI.Sampled     = true;
-            m_OffscreenRenderTarget      = New<OffscreenRenderTarget>( mEngineLoop->GetGraphicContext(), l_RenderTargetCI );
-            m_ViewportRenderContext      = LTSE::Graphics::RenderContext( mEngineLoop->GetGraphicContext(), m_OffscreenRenderTarget );
+            mOffscreenRenderTarget      = New<OffscreenRenderTarget>( mEngineLoop->GetGraphicContext(), l_RenderTargetCI );
+            mViewportRenderContext      = LTSE::Graphics::RenderContext( mEngineLoop->GetGraphicContext(), mOffscreenRenderTarget );
         }
         else
         {
-            m_OffscreenRenderTarget->Resize( m_ViewportWidth, m_ViewportHeight );
+            mOffscreenRenderTarget->Resize( mViewportWidth, mViewportHeight );
         }
 
-        m_OffscreenRenderTargetTexture = New<Graphics::Texture2D>(
-            mEngineLoop->GetGraphicContext(), TextureDescription{}, m_OffscreenRenderTarget->GetOutputImage() );
+        mOffscreenRenderTargetTexture = New<Graphics::Texture2D>( mEngineLoop->GetGraphicContext(), TextureDescription{},
+                                                                   mOffscreenRenderTarget->GetOutputImage() );
 
-        if( !m_OffscreenRenderTargetDisplayHandle.Handle )
+        if( !mOffscreenRenderTargetDisplayHandle.Handle )
         {
-            m_OffscreenRenderTargetDisplayHandle = mEngineLoop->UIContext()->CreateTextureHandle( m_OffscreenRenderTargetTexture );
-            mEditorWindow.UpdateSceneViewport( m_OffscreenRenderTargetDisplayHandle );
+            mOffscreenRenderTargetDisplayHandle = mEngineLoop->UIContext()->CreateTextureHandle( mOffscreenRenderTargetTexture );
+            mEditorWindow.UpdateSceneViewport( mOffscreenRenderTargetDisplayHandle );
         }
         else
         {
-            m_OffscreenRenderTargetDisplayHandle.Handle->Write( m_OffscreenRenderTargetTexture, 0 );
+            mOffscreenRenderTargetDisplayHandle.Handle->Write( mOffscreenRenderTargetTexture, 0 );
         }
 
-        if( m_WorldRenderer )
+        if( mWorldRenderer )
         {
-            m_WorldRenderer->View.Projection = math::Perspective(
-                90.0_degf, static_cast<float>( m_ViewportWidth ) / static_cast<float>( m_ViewportHeight ), 0.01f, 100000.0f );
-            m_WorldRenderer->View.Projection[1][1] *= -1.0f;
+            mWorldRenderer->View.Projection = math::Perspective(
+                90.0_degf, static_cast<float>( mViewportWidth ) / static_cast<float>( mViewportHeight ), 0.01f, 100000.0f );
+            mWorldRenderer->View.Projection[1][1] *= -1.0f;
         }
     }
 
     bool BaseEditorApplication::RenderUI( ImGuiIO &io )
     {
         bool o_RequestQuit = false;
-        if( m_ShouldRebuildViewport )
+        if( mShouldRebuildViewport )
         {
             RebuildOutputFramebuffer();
-            m_ShouldRebuildViewport = false;
+            mShouldRebuildViewport = false;
         }
 
         mEditorWindow.mEngineLoop = mEngineLoop;
         // mEditorWindow.SensorModel    = m_SensorController;
-        mEditorWindow.WorldRenderer  = m_WorldRenderer;
+        mEditorWindow.WorldRenderer  = mWorldRenderer;
         mEditorWindow.GraphicContext = mEngineLoop->GetGraphicContext();
 
         o_RequestQuit = mEditorWindow.Display();
-        OnUI();
+        // OnUI();
 
         auto l_WorkspaceAreaSize = mEditorWindow.GetWorkspaceAreaSize();
-        if( ( m_ViewportWidth != l_WorkspaceAreaSize.x ) || ( m_ViewportHeight != l_WorkspaceAreaSize.y ) )
+        if( ( mViewportWidth != l_WorkspaceAreaSize.x ) || ( mViewportHeight != l_WorkspaceAreaSize.y ) )
         {
-            m_ViewportWidth         = l_WorkspaceAreaSize.x;
-            m_ViewportHeight        = l_WorkspaceAreaSize.y;
-            m_ShouldRebuildViewport = true;
+            mViewportWidth         = l_WorkspaceAreaSize.x;
+            mViewportHeight        = l_WorkspaceAreaSize.y;
+            mShouldRebuildViewport = true;
         }
 
         if( o_RequestQuit ) return true;
@@ -161,95 +116,19 @@ namespace LTSE::Editor
 
     void BaseEditorApplication::Init()
     {
-        // m_SensorController = a_SensorToControl;
-
-        mEngineLoop->SetApplicationName( ApplicationName );
-        mEngineLoop->PreInit( 0, nullptr );
-
-        // get cwd
-        {
-            char buff[MAX_PATH];
-            _getcwd( buff, MAX_PATH );
-            fs::path lCwd = std::string( buff );
-
-            LTSE::Logging::Info( "Current working directory is: '{}'", lCwd.string() );
-
-            // ConfigurationRoot = lCwd;
-            // if( fs::exists( lCwd / "SensorConfiguration.yaml" ) )
-            // {
-            //     LTSE::Logging::Info( "Current working directory set to: '{}'", ConfigurationRoot.string() );
-            // }
-            // else
-            // {
-            //     while( !ConfigurationRoot.empty() && ( ConfigurationRoot != ConfigurationRoot.root_path() ) && !fs::exists(
-            //     ConfigurationRoot / "SensorConfiguration.yaml" ) )
-            //     {
-            //         ConfigurationRoot = ConfigurationRoot.parent_path();
-            //         LTSE::Logging::Info( "Looking for sensor configuration in: '{}'", ConfigurationRoot.string() );
-            //     }
-
-            //     if( fs::exists( ConfigurationRoot / "SensorConfiguration.yaml" ) )
-            //     {
-            //         LTSE::Logging::Info( "Current working directory set to: '{}'", ConfigurationRoot.string() );
-            //     }
-            //     else
-            //     {
-            //         ConfigurationRoot = lCwd;
-            //         LTSE::Logging::Error( "The file 'SensorConfiguration.yaml' was not found in this folder or any of its parents."
-            //         ); exit( 2 );
-            //     }
-            // }
-        }
-
-        // Create Saved, Saved/Logs
-        if( !fs::exists( ConfigurationRoot / "Saved" / "Logs" ) ) fs::create_directories( ConfigurationRoot / "Saved" / "Logs" );
-
-        // Configure logger to send messages to saved/logs/EditorLogs.txt
-        LTSE::Logging::Info(
-            "Log file will be written to '{}'", ( ConfigurationRoot / "Saved" / "Logs" / "EditorLogs.txt" ).string() );
-        LTSE::Logging::SetLogOutputFile( ConfigurationRoot / "Saved" / "Logs" / "EditorLogs.txt" );
-
-        ConfigurationFile = ConfigurationRoot / "EditorConfiguration.yaml";
-        if( fs::exists( ConfigurationFile ) )
-            LoadConfiguration();
-        else
-            SaveConfiguration();
-
-        ScriptManager::Initialize();
-
-        // SensorConfigurationFile = ConfigurationRoot / "SensorConfiguration.yaml";
-        // if( fs::exists( SensorConfigurationFile ) )
-        // {
-        //     LTSE::Logging::Info( "Loading sensor configuration from '{}'", SensorConfigurationFile.string() );
-        // }
-        // else
-        // {
-        //     LTSE::Logging::Info( "Sensor configuration not found.", SensorConfigurationFile.string() );
-        // }
-        // LoadSensorConfiguration();
-
-        mEngineLoop->SetInitialWindowSize( WindowSize );
-        mEngineLoop->SetInitialWindowPosition( WindowPosition );
-        mEngineLoop->SetImGuiConfigurationFile( ( ConfigurationRoot / "Saved" / "imgui.ini" ).string() );
-        mEngineLoop->Init();
-
-        mEngineLoop->RenderDelegate.connect<&BaseEditorApplication::RenderScene>( *this );
-        mEngineLoop->UIDelegate.connect<&BaseEditorApplication::RenderUI>( *this );
-        mEngineLoop->UpdateDelegate.connect<&BaseEditorApplication::Update>( *this );
-
         mEditorWindow                 = EditorWindow( mEngineLoop->GetGraphicContext(), mEngineLoop->UIContext() );
         mEditorWindow.ApplicationIcon = ICON_FA_CODEPEN;
 
         RebuildOutputFramebuffer();
-        m_World         = New<Scene>( mEngineLoop->GetGraphicContext(), mEngineLoop->UIContext() );
-        m_WorldRenderer = New<SceneRenderer>( m_World, m_ViewportRenderContext, m_OffscreenRenderTarget->GetRenderPass() );
+        mWorld         = New<Scene>( mEngineLoop->GetGraphicContext(), mEngineLoop->UIContext() );
+        mWorldRenderer = New<SceneRenderer>( mWorld, mViewportRenderContext, mOffscreenRenderTarget->GetRenderPass() );
 
-        mEditorWindow.World       = m_World;
-        mEditorWindow.ActiveWorld = m_World;
+        mEditorWindow.World       = mWorld;
+        mEditorWindow.ActiveWorld = mWorld;
 
         {
             // Add sensor entity to the scene
-            mEditorWindow.Sensor = m_World->Create( "Sensor", m_World->Root );
+            mEditorWindow.Sensor = mWorld->Create( "Sensor", mWorld->Root );
             mEditorWindow.Sensor.Add<sNodeTransformComponent>();
             mEditorWindow.Sensor.Add<sBehaviourComponent>();
 
@@ -262,25 +141,9 @@ namespace LTSE::Editor
             mEditorWindow.ActiveSensor = mEditorWindow.Sensor;
         }
 
-        m_WorldRenderer->RenderCoordinateGrid = true;
-        m_WorldRenderer->View.CameraPosition  = math::vec3( 0.0f, 1.0f, 7.5f );
-        m_WorldRenderer->View.ModelFraming    = math::mat4( 0.5f );
-        m_WorldRenderer->View.View = math::Inverse( math::Translate( math::mat4( 1.0f ), m_WorldRenderer->View.CameraPosition ) );
+        mWorldRenderer->RenderCoordinateGrid = true;
+        mWorldRenderer->View.CameraPosition  = math::vec3( 0.0f, 1.0f, 7.5f );
+        mWorldRenderer->View.ModelFraming    = math::mat4( 0.5f );
+        mWorldRenderer->View.View = math::Inverse( math::Translate( math::mat4( 1.0f ), mWorldRenderer->View.CameraPosition ) );
     }
-
-    uint32_t BaseEditorApplication::Run()
-    {
-        while( mEngineLoop->Tick() )
-        {
-        }
-
-        SaveConfiguration();
-
-        mEngineLoop->Shutdown();
-    
-        ScriptManager::Shutdown();
-
-        return 0;
-    }
-
 } // namespace LTSE::Editor
--- Source/Editor/BaseEditorApplication.h
+++ Source/Editor/BaseEditorApplication.h
@@ -10,13 +10,11 @@
 #include "Core/GraphicContext//UI/UIContext.h"
 #include "Core/Platform/EngineLoop.h"
 
-
+#include "Core/GraphicContext//GraphicContext.h"
 #include "Scene/EnvironmentSampler/EnvironmentSampler.h"
 #include "Scene/EnvironmentSampler/PointCloudVisualizer.h"
-#include "Core/GraphicContext//GraphicContext.h"
 #include "Scene/Renderer/SceneRenderer.h"
 #include "Scene/Scene.h"
-
 
 // #include "LidarSensorModel/SensorDeviceBase.h"
 #include "TensorOps/Scope.h"
@@ -29,60 +27,48 @@ namespace LTSE::Editor
 
     using namespace LTSE::Core;
     using namespace LTSE::Graphics;
-    // using namespace LTSE::SensorModel;
-    // using namespace LTSE::SensorModel::Dev;
     using namespace LTSE::TensorOps;
 
     class BaseEditorApplication
     {
       public:
+        Ref<EngineLoop> mEngineLoop = nullptr;
+
         EditorWindow mEditorWindow;
-        Ref<EngineLoop> mEngineLoop      = nullptr;
-        fs::path ConfigurationRoot       = "";
-        fs::path ConfigurationFile       = "";
-        fs::path SensorConfigurationFile = "";
-        std::string ApplicationName      = "Sensor Model Editor";
-        std::string ImGuiIniFile         = "imgui_config.ini";
-        math::ivec2 WindowSize           = { 1920, 1080 };
-        math::ivec2 WindowPosition       = { 100, 100 };
+        fs::path     ConfigurationRoot       = "";
+        fs::path     ConfigurationFile       = "";
+        fs::path     SensorConfigurationFile = "";
+        std::string  ApplicationName         = "Sensor Model Editor";
+        std::string  ImGuiIniFile            = "imgui_config.ini";
+        math::ivec2  WindowSize              = { 1920, 1080 };
+        math::ivec2  WindowPosition          = { 100, 100 };
 
       public:
-        BaseEditorApplication();
+        BaseEditorApplication() = default;
+        BaseEditorApplication( Ref<EngineLoop> aEngineLoop );
+
         ~BaseEditorApplication() = default;
 
         void Init();
 
         void RenderScene();
-        virtual void Update( Timestep ts ) = 0;
+        void Update( Timestep ts );
         bool RenderUI( ImGuiIO &io );
-
-        uint32_t Run();
-
-        virtual void LoadSensorConfiguration() = 0;
-        virtual void SaveSensorConfiguration() = 0;
-        virtual void OnUI()                    = 0;
-
-      protected:
-        void LoadConfiguration();
-        void SaveConfiguration();
 
       protected:
         void RebuildOutputFramebuffer();
 
       protected:
-        uint32_t m_ViewportHeight          = 1;
-        uint32_t m_ViewportWidth           = 1;
-        bool m_ShouldRebuildViewport       = true;
-        Ref<Scene> m_World                 = nullptr;
-        Ref<SceneRenderer> m_WorldRenderer = nullptr;
+        uint32_t           mViewportHeight        = 1;
+        uint32_t           mViewportWidth         = 1;
+        bool               mShouldRebuildViewport = true;
+        Ref<Scene>         mWorld                 = nullptr;
+        Ref<SceneRenderer> mWorldRenderer         = nullptr;
 
-        RenderContext m_ViewportRenderContext{};
-        Ref<OffscreenRenderTarget> m_OffscreenRenderTarget      = nullptr;
-        Ref<Graphics::Texture2D> m_OffscreenRenderTargetTexture = nullptr;
-        // Ref<SensorDeviceBase> m_SensorController                = nullptr;
-
-        ImageHandle m_OffscreenRenderTargetDisplayHandle{};
-        Entity m_SensorEntity{};
+        RenderContext              mViewportRenderContext{};
+        Ref<OffscreenRenderTarget> mOffscreenRenderTarget = nullptr;
+        ImageHandle                mOffscreenRenderTargetDisplayHandle{};
+        Ref<Graphics::Texture2D>   mOffscreenRenderTargetTexture = nullptr;
     };
 
 } // namespace LTSE::Editor
