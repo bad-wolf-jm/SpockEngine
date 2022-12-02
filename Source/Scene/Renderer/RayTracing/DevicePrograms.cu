19ed08cd28e25b4e7237f7dfe818660cf95f9f3b
bad-wolf-jm <jm.albert@gmail.com>
2022-12-02 15:42:26 -0500

    Added PBR shading functions for later use

--- Source/Scene/Renderer/RayTracing/DevicePrograms.cu
+++ Source/Scene/Renderer/RayTracing/DevicePrograms.cu
@@ -98,6 +98,42 @@ namespace SE::Core
         return normalize( aInNormal );
     }
 
+    constexpr float PI = 3.14159265359;
+
+    static SE_CUDA_INLINE SE_CUDA_DEVICE_FUNCTION_DEF float DistributionGGX( vec3 N, vec3 H, float roughness )
+    {
+        float a      = roughness * roughness;
+        float a2     = a * a;
+        float NdotH  = max( dot( N, H ), 0.0 );
+        float NdotH2 = NdotH * NdotH;
+
+        float nom   = a2;
+        float denom = ( NdotH2 * ( a2 - 1.0 ) + 1.0 );
+        denom       = PI * denom * denom;
+
+        return nom / denom;
+    }
+
+    static SE_CUDA_INLINE SE_CUDA_DEVICE_FUNCTION_DEF float GeometrySchlickGGX( float NdotV, float roughness )
+    {
+        float r     = ( roughness + 1.0 );
+        float k     = ( r * r ) / 8.0;
+        float nom   = NdotV;
+        float denom = NdotV * ( 1.0 - k ) + k;
+
+        return nom / denom;
+    }
+
+    static SE_CUDA_INLINE SE_CUDA_DEVICE_FUNCTION_DEF float GeometrySmith( vec3 N, vec3 V, vec3 L, float roughness )
+    {
+        float NdotV = max( dot( N, V ), 0.0 );
+        float NdotL = max( dot( N, L ), 0.0 );
+        float ggx2  = GeometrySchlickGGX( NdotV, roughness );
+        float ggx1  = GeometrySchlickGGX( NdotL, roughness );
+
+        return ggx1 * ggx2;
+    }
+
     extern "C" CUDA_KERNEL_DEFINITION void __closesthit__radiance()
     {
         const sTriangleMeshSBTData &sbtData = *(const sTriangleMeshSBTData *)optixGetSbtDataPointer();
@@ -196,9 +232,7 @@ namespace SE::Core
 
         prd.mPixelNormal = Ns;
         prd.mPixelAlbedo = diffuseColor;
-        // prd.mPixelColor  = math::vec3{lMaterialData.mBaseColorTextureID / 255.0, lMaterialData.mBaseColorTextureID / 255.0,
-        // lMaterialData.mBaseColorTextureID / 255.0};//pixelColor;
-        prd.mPixelColor = pixelColor;
+        prd.mPixelColor  = pixelColor;
     }
 
     extern "C" CUDA_KERNEL_DEFINITION void __anyhit__radiance() {}
