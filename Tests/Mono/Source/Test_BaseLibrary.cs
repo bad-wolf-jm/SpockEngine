b30fad2fca0689aa364efec83d9c445ff65b23c5
Jean-Martin Albert <jm.albert@gmail.com>
2022-12-20 16:46:54 -0500

    Unit tests for vec4

--- Tests/Mono/Source/Test_BaseLibrary.cs
+++ Tests/Mono/Source/Test_BaseLibrary.cs
@@ -3,8 +3,10 @@ using SpockEngine.Math;
 
 namespace SEUnitTest
 {
-    public class VectorTests
+    public class Vector3Tests
     {
+        public static vec3 Constructor(float x, float y, float z) { return new vec3(x, y, z); }
+
         public static vec3 Add(vec3 x, vec3 y) { return x + y; }
         public static vec3 Subtract(vec3 x, vec3 y) { return x - y; }
         public static vec3 Divide0(vec3 x, float y) { return x / y; }
@@ -18,6 +20,26 @@ namespace SEUnitTest
         public static float Norm1(vec3 x) { return x.Norm1; }
         public static float Norm2(vec3 x) { return x.Norm2; }
         public static vec3 Normalized(vec3 x) { return x.Normalized; }
-
     }
+
+    public class Vector4Tests
+    {
+        public static vec4 Constructor0(float x, float y, float z, float w) { return new vec4(x, y, z, w); }
+        public static vec4 Constructor1(vec3 xyz, float w) { return new vec4(xyz, w); }
+        public static vec4 Constructor2(vec3 xyz) { return new vec4(xyz); }
+        public static vec3 Projection(vec4 v) { return new vec3(v); }
+        public static vec4 Add(vec4 x, vec4 y) { return x + y; }
+        public static vec4 Subtract(vec4 x, vec4 y) { return x - y; }
+        public static vec4 Divide0(vec4 x, float y) { return x / y; }
+        public static vec4 Divide1(float x, vec4 y) { return x / y; }
+        public static vec4 Multiply0(vec4 x, float y) { return x * y; }
+        public static vec4 Multiply1(float x, vec4 y) { return x * y; }
+        public static float Dot(vec4 x, vec4 y) { return x.Dot(y); }
+        public static float Length(vec4 x) { return x.Length; }
+        public static float Norm(vec4 x) { return x.Norm; }
+        public static float Norm1(vec4 x) { return x.Norm1; }
+        public static float Norm2(vec4 x) { return x.Norm2; }
+        public static vec4 Normalized(vec4 x) { return x.Normalized; }
+    }
+
 }
--- Tests/Test_MonoScripting.cpp
+++ Tests/Test_MonoScripting.cpp
@@ -53,14 +53,27 @@ inline math::vec3 ToVec3( MonoObject *x )
     return math::make_vec3( lV );
 }
 
-TEST_CASE( "Vector addition", "[MONO_SCRIPTING]" )
+inline math::vec4 ToVec4( MonoObject *x )
+{
+    float *lV = (float *)mono_object_unbox( x );
+    return math::make_vec4( lV );
+}
+
+TEST_CASE( "Vector3 operations", "[MONO_SCRIPTING]" )
 {
     InitializeMonoscripting( "C:\\GitLab\\SpockEngine\\Tests\\Mono\\Build\\Debug\\MonoscriptingTest.dll" );
 
-    auto lVectorTest = MonoScriptClass( "SEUnitTest", "VectorTests", false );
+    auto lVectorTest = MonoScriptClass( "SEUnitTest", "Vector3Tests", false );
 
-    auto lX = math::vec3{ 1.2f, 3.4f, 5.6f };
-    auto lY = math::vec3{ 3.4f, 1.2f, 5.6f };
+    float lS = 1.234f;
+    auto  lX = math::vec3{ 1.2f, 3.4f, 5.6f };
+    auto  lY = math::vec3{ 3.4f, 5.6f, 7.8f };
+
+    {
+        auto lR = lVectorTest.CallMethod( "Constructor", lX.x, lX.y, lX.z );
+        auto lZ = ToVec3( lR );
+        REQUIRE( lZ == lX );
+    }
 
     {
         auto lR = lVectorTest.CallMethod( "Add", lX, lY );
@@ -75,30 +88,26 @@ TEST_CASE( "Vector addition", "[MONO_SCRIPTING]" )
     }
 
     {
-        float lS = 1.234f;
-        auto  lR = lVectorTest.CallMethod( "Divide0", lX, lS );
-        auto  lZ = ToVec3( lR );
+        auto lR = lVectorTest.CallMethod( "Divide0", lX, lS );
+        auto lZ = ToVec3( lR );
         REQUIRE( lZ == ( lX / lS ) );
     }
 
     {
-        float lS = 1.234f;
-        auto  lR = lVectorTest.CallMethod( "Divide1", lS, lY );
-        auto  lZ = ToVec3( lR );
+        auto lR = lVectorTest.CallMethod( "Divide1", lS, lY );
+        auto lZ = ToVec3( lR );
         REQUIRE( lZ == ( lS / lY ) );
     }
 
     {
-        float lS = 32.234f;
-        auto  lR = lVectorTest.CallMethod( "Multiply0", lX, lS );
-        auto  lZ = ToVec3( lR );
+        auto lR = lVectorTest.CallMethod( "Multiply0", lX, lS );
+        auto lZ = ToVec3( lR );
         REQUIRE( lZ == ( lX * lS ) );
     }
 
     {
-        float lS = 21.234f;
-        auto  lR = lVectorTest.CallMethod( "Multiply1", lS, lY );
-        auto  lZ = ToVec3( lR );
+        auto lR = lVectorTest.CallMethod( "Multiply1", lS, lY );
+        auto lZ = ToVec3( lR );
         REQUIRE( lZ == ( lS * lY ) );
     }
 
@@ -141,6 +150,112 @@ TEST_CASE( "Vector addition", "[MONO_SCRIPTING]" )
     {
         auto lR = lVectorTest.CallMethod( "Normalized", lX );
         auto lZ = ToVec3( lR );
+        REQUIRE( math::length( lZ - math::normalize( lX ) ) < 0.0000001f );
+    }
+}
+
+TEST_CASE( "Vector4 operations", "[MONO_SCRIPTING]" )
+{
+    InitializeMonoscripting( "C:\\GitLab\\SpockEngine\\Tests\\Mono\\Build\\Debug\\MonoscriptingTest.dll" );
+
+    auto lVectorTest = MonoScriptClass( "SEUnitTest", "Vector4Tests", false );
+
+    float lS = 1.234f;
+    auto  lX = math::vec4{ 1.2f, 3.4f, 5.6f, 4.2f };
+    auto  lY = math::vec4{ 3.4f, 5.6f, 7.8f, 7.5f };
+
+    {
+        auto lR = lVectorTest.CallMethod( "Constructor0", lX.x, lX.y, lX.z, lX.w );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == lX );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Constructor1", math::vec3( lX ), lX.w );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == lX );
+    }
+    {
+        auto lR = lVectorTest.CallMethod( "Constructor2", math::vec3( lX ) );
+        auto lZ = ToVec4( lR );
+        REQUIRE( (( math::vec3( lZ ) == math::vec3( lX ) ) && ( lZ.w == 0.0f )) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Projection", lX );
+        auto lZ = ToVec3( lR );
+        REQUIRE( lZ == math::vec3( lX ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Add", lX, lY );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == ( lX + lY ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Subtract", lX, lY );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == ( lX - lY ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Divide0", lX, lS );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == ( lX / lS ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Divide1", lS, lY );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == ( lS / lY ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Multiply0", lX, lS );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == ( lX * lS ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Multiply1", lS, lY );
+        auto lZ = ToVec4( lR );
+        REQUIRE( lZ == ( lS * lY ) );
+    }
+
+    {
+        auto  lR = lVectorTest.CallMethod( "Dot", lX, lY );
+        float lV = ToFloat( lR );
+        REQUIRE( lV == ( math::dot( lX, lY ) ) );
+    }
+
+    {
+        auto  lR = lVectorTest.CallMethod( "Length", lX );
+        float lV = ToFloat( lR );
+        REQUIRE( lV == ( math::length( lX ) ) );
+    }
+
+    {
+        auto  lR = lVectorTest.CallMethod( "Norm", lX );
+        float lV = ToFloat( lR );
+        REQUIRE( lV == ( math::length( lX ) ) );
+    }
+
+    {
+        auto  lR = lVectorTest.CallMethod( "Norm1", lX );
+        float lV = ToFloat( lR );
+        REQUIRE( lV == ( math::abs( lX.x ) + math::abs( lX.y ) + math::abs( lX.z ) + math::abs( lX.w ) ) );
+    }
+
+    {
+        auto  lR = lVectorTest.CallMethod( "Norm2", lX );
+        float lV = ToFloat( lR );
+        REQUIRE( lV == ( math::length( lX ) ) );
+    }
+
+    {
+        auto lR = lVectorTest.CallMethod( "Normalized", lX );
+        auto lZ = ToVec4( lR );
         REQUIRE( math::length( lZ - math::normalize( lX ) ) < 0.0000001f );
     }
 }
