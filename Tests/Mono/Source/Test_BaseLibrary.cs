3d6e94faadb17399ff0482c20a0c35f57771d5c8
Jean-Martin Albert <jm.albert@gmail.com>
2022-12-20 16:03:19 -0500

    Code formatting

--- Tests/Mono/Source/Test_BaseLibrary.cs
+++ Tests/Mono/Source/Test_BaseLibrary.cs
@@ -9,15 +9,10 @@ namespace SEUnitTest
         public static vec3 Subtract(vec3 x, vec3 y) { return x - y; }
         public static vec3 Divide0(vec3 x, float y) { return x / y; }
         public static vec3 Divide1(float x, vec3 y) { return x / y; }
-
         public static vec3 Multiply0(vec3 x, float y) { return x * y; }
         public static vec3 Multiply1(float x, vec3 y) { return x * y; }
-
-
         public static float Dot(vec3 x, vec3 y) { return x.Dot(y); }
         public static vec3 Cross(vec3 x, vec3 y) { return x.Cross(y); }
-
-
         public static float Length(vec3 x) { return x.Length; }
         public static float Norm(vec3 x) { return x.Norm; }
         public static float Norm1(vec3 x) { return x.Norm1; }
