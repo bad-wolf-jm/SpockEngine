a7f365c77b56a3e610a98478345585684752ba9b
Jean-Martin Albert <jm.albert@gmail.com>
2023-01-20 14:10:06 -0500

    Added renderer for shadow maps

---
+++ Source/Scene/Renderer/ShadowSceneRenderer.cpp
@@ -0,0 +1,158 @@
+#include "ShadowSceneRenderer.h"
+
+#include <chrono>
+#include <gli/gli.hpp>
+
+#include "Graphics/Vulkan/VkPipeline.h"
+
+#include "Scene/Components/VisualHelpers.h"
+#include "Scene/Primitives/Primitives.h"
+#include "Scene/VertexData.h"
+
+#include "Core/Profiling/BlockTimer.h"
+
+#include "Core/Logging.h"
+#include "Core/Resource.h"
+
+#include "MeshRenderer.h"
+#include "ParticleSystemRenderer.h"
+
+namespace SE::Core
+{
+
+    using namespace math;
+    using namespace SE::Core::EntityComponentSystem::Components;
+    using namespace SE::Core::Primitives;
+
+    Ref<DescriptorSetLayout> ShadowMeshRenderer::GetCameraSetLayout( Ref<VkGraphicContext> aGraphicContext )
+    {
+        DescriptorSetLayoutCreateInfo lCameraBindLayout{};
+        lCameraBindLayout.Bindings = {
+            DescriptorBindingInfo{ 0, eDescriptorType::UNIFORM_BUFFER, { eShaderStageTypeFlags::VERTEX } } };
+
+        return New<DescriptorSetLayout>( aGraphicContext, lCameraBindLayout );
+    }
+
+    std::vector<Ref<DescriptorSetLayout>> ShadowMeshRenderer::GetDescriptorSetLayout() { return { CameraSetLayout }; }
+
+    std::vector<sPushConstantRange> ShadowMeshRenderer::GetPushConstantLayout() { return {}; };
+
+    ShadowMeshRenderer::ShadowMeshRenderer( Ref<VkGraphicContext> aGraphicContext, ShadowMeshRendererCreateInfo const &aCreateInfo )
+        : SceneRenderPipeline<VertexData>( aGraphicContext )
+    {
+
+        SceneRenderPipelineCreateInfo lCreateInfo{};
+        lCreateInfo.Opaque       = true;
+        lCreateInfo.LineWidth    = 1.0f;
+        lCreateInfo.VertexShader = "Shaders\\Shadow.vert.spv";
+        lCreateInfo.RenderPass   = aCreateInfo.RenderPass;
+
+        CameraSetLayout = GetCameraSetLayout( mGraphicContext );
+
+        Initialize( lCreateInfo );
+    }
+
+    ShadowSceneRenderer::ShadowSceneRenderer( Ref<VkGraphicContext> aGraphicContext )
+        : ASceneRenderer( aGraphicContext, eColorFormat::UNDEFINED, 1 )
+    {
+        mSceneDescriptors = New<DescriptorSet>( mGraphicContext, ShadowMeshRenderer::GetCameraSetLayout( mGraphicContext ) );
+
+        mCameraUniformBuffer =
+            New<VkGpuBuffer>( mGraphicContext, eBufferType::UNIFORM_BUFFER, true, true, true, true, sizeof( ShadowMatrices ) );
+        mSceneDescriptors->Write( mCameraUniformBuffer, false, 0, sizeof( ShadowMatrices ), 0 );
+    }
+
+    void ShadowSceneRenderer::ResizeOutput( uint32_t aOutputWidth, uint32_t aOutputHeight ) {}
+
+    void ShadowSceneRenderer::Update( Ref<Scene> aWorld )
+    {
+        ASceneRenderer::Update( aWorld );
+
+        // View.PointLightCount = mPointLights.size();
+        // for( uint32_t i = 0; i < View.PointLightCount; i++ ) View.PointLights[i] = mPointLights[i];
+
+        // View.DirectionalLightCount = mDirectionalLights.size();
+        if( mDirectionalLights.size() != mDirectionalShadowMaps.size() )
+        {
+            mDirectionalShadowMaps.clear();
+            for( uint32_t i = 0; i < mDirectionalLights.size(); i++ )
+            {
+                sRenderTargetDescription lRenderTargetSpec{};
+                lRenderTargetSpec.mWidth       = 1024;
+                lRenderTargetSpec.mHeight      = 1024;
+                lRenderTargetSpec.mSampleCount = 1;
+                mDirectionalShadowMaps.emplace_back();
+                mDirectionalShadowMaps.back() = New<VkRenderTarget>( mGraphicContext, lRenderTargetSpec );
+
+                sAttachmentDescription lAttachmentCreateInfo{};
+                lAttachmentCreateInfo.mIsSampled   = true;
+                lAttachmentCreateInfo.mIsPresented = false;
+                lAttachmentCreateInfo.mLoadOp      = eAttachmentLoadOp::CLEAR;
+                lAttachmentCreateInfo.mStoreOp     = eAttachmentStoreOp::STORE;
+
+                lAttachmentCreateInfo.mType       = eAttachmentType::DEPTH;
+                lAttachmentCreateInfo.mClearColor = { 1.0f, 0.0f, 0.0f, 0.0f };
+                mDirectionalShadowMaps.back()->AddAttachment( "DEPTH_STENCIL", lAttachmentCreateInfo );
+
+                mDirectionalShadowMaps.back()->Finalize();
+
+                mDirectionalShadowMapRenderContext.emplace_back( mGraphicContext, mDirectionalShadowMaps.back() );
+            }
+        }
+
+        if( mDirectionalShadowMapRenderContext.size() > 0 )
+        {
+            ShadowMeshRendererCreateInfo lCreateInfo{};
+            lCreateInfo.RenderPass = mDirectionalShadowMapRenderContext.back().GetRenderPass();
+            mRenderPipeline        = ShadowMeshRenderer( mGraphicContext, lCreateInfo );
+        }
+
+        // View.SpotlightCount = mSpotlights.size();
+        // for( uint32_t i = 0; i < View.SpotlightCount; i++ ) View.Spotlights[i] = mSpotlights[i];
+    }
+
+    void ShadowSceneRenderer::Render()
+    {
+        SE_PROFILE_FUNCTION();
+
+        if( !mScene ) return;
+        if( !mRenderPipeline.Pipeline ) return;
+
+        uint32_t lLightIndex = 0;
+        for( auto &lContext : mDirectionalShadowMapRenderContext )
+        {
+            // clang-format off
+            const float aEntries[] = { 1.0f,  0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f, 0.0f,  0.0f, 0.0f, 1.0f };
+            math::mat4  lClip = math::MakeMat4( aEntries );
+            // clang-format on
+
+            math::mat4 lProjection =
+                math::Orthogonal( math::vec2{ -10.0f, 10.0f }, math::vec2{ -10.0f, 10.0f }, math::vec2{ 0.0f, 10.0f } );
+            math::mat4 lView = math::LookAt( -mDirectionalLights[lLightIndex].Direction, math::vec3{ 0.0f, 0.0f, 0.0f },
+                                             math::vec3{ 0.0f, 1.0f, 0.0f } );
+            View.mMVP        = lClip * lProjection * lView;
+            mCameraUniformBuffer->Write( View );
+
+            lContext.BeginRender();
+            for( auto &lPipelineData : mOpaqueMeshQueue )
+            {
+                lContext.Bind( mRenderPipeline.Pipeline );
+                lContext.Bind( mSceneDescriptors, 0, -1 );
+
+                if( !lPipelineData.mVertexBuffer || !lPipelineData.mIndexBuffer ) continue;
+                lContext.Bind( lPipelineData.mVertexBuffer, lPipelineData.mIndexBuffer );
+
+                lContext.Draw( lPipelineData.mIndexCount, lPipelineData.mIndexOffset, lPipelineData.mVertexOffset, 1, 0 );
+            }
+
+            lContext.EndRender();
+            lLightIndex++;
+        }
+    }
+
+    Ref<VkTexture2D> ShadowSceneRenderer::GetOutputImage()
+    {
+        //
+        return mGeometryRenderTarget->GetAttachment( "OUTPUT" );
+    }
+} // namespace SE::Core
