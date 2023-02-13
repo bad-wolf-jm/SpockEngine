28a998b6562217e2128d6f37caaa72e56a230b74
Jean-Martin Albert <jm.albert@gmail.com>
2023-02-13 16:16:45 -0500

    Moved some component editing functions to the collection module

---
+++ Source/Core/EntityCollection/EditComponent.cpp
@@ -0,0 +1,34 @@
+#include "EditComponent.h"
+
+#include "UI/UI.h"
+#include "UI/Widgets.h"
+
+namespace SE::Core
+{
+    bool EditComponent( sActorComponent &aComponent )
+    {
+        UI::Text( "Component class:" );
+        UI::Text( "{}", aComponent.mClassFullName );
+
+        for( const auto &[name, field] : aComponent.mClass.GetFields() )
+        {
+            UI::Text( "{}", name );
+        }
+
+        return false;
+    }
+
+    bool EditComponent( sHUDComponent &aComponent )
+    {
+        UI::Text( "Component class:" );
+        UI::Text( "{}", aComponent.mClassFullName );
+
+        for( const auto &[name, field] : aComponent.mClass.GetFields() )
+        {
+            UI::Text( "{}", name );
+        }
+
+        return false;
+    }
+
+} // namespace SE::Core
---
+++ Source/Core/EntityCollection/EditComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Collection.h"
+
+namespace SE::Core
+{
+    bool EditComponent( sActorComponent &aComponent );
+    bool EditComponent( sHUDComponent &aComponent );
+} // namespace SE::Core
