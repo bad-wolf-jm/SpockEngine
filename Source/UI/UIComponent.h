d830f5e2595b88d446416b3d3b685bc524287a89
Jean-Martin Albert <jm.albert@gmail.com>
2023-02-16 11:18:50 -0500

    Added UI component base class

---
+++ Source/UI/UIComponent.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "UI.h"
+
+namespace SE::Core
+{
+    class UIComponent
+    {
+      public:
+        bool mIsVisible     = true;
+        bool mIsEnabled     = true;
+        bool mAllowDragDrop = true;
+
+      public:
+        UIComponent()  = default;
+        ~UIComponent() = default;
+
+        void Update( ImVec2 aPosition, ImVec2 aSize );
+
+        virtual void PushStyles()   = 0;
+        virtual void PopStyles()    = 0;
+        virtual void RequiredSize() = 0;
+
+      protected:
+        virtual void DrawContent( ImVec2 aPosition, ImVec2 aSize ) = 0;
+    };
+} // namespace SE::Core
