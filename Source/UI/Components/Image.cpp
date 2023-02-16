eed81cd9f035c5b0367d76087406e8ae68c9061c
Jean-Martin Albert <jm.albert@gmail.com>
2023-02-16 16:32:26 -0500

    Derive from base image class

--- Source/UI/Components/Image.cpp
+++ Source/UI/Components/Image.cpp
@@ -3,50 +3,8 @@
 namespace SE::Core
 {
     UIImage::UIImage( Ref<UIContext> aUIContext, fs::path const &aImagePath, math::vec2 aSize )
-        : mImagePath{ aImagePath }
-        , mUIContext{ aUIContext }
-        , mSize{ aSize.x, aSize.y }
+        : UIBaseImage(aUIContext, aImagePath, aSize)
     {
-        SetImage( aImagePath );
-    }
-
-    void UIImage::PushStyles() {}
-    void UIImage::PopStyles() {}
-
-    UIImage &UIImage::SetImage( fs::path const &aImagePath )
-    {
-        SE::Core::sTextureCreateInfo lTextureCreateInfo{};
-        TextureData2D                lTextureData( lTextureCreateInfo, aImagePath );
-        sTextureSamplingInfo         lSamplingInfo{};
-        SE::Core::TextureSampler2D   lTextureSampler = SE::Core::TextureSampler2D( lTextureData, lSamplingInfo );
-
-        auto lTexture = New<VkTexture2D>( mUIContext->GraphicContext(), lTextureData );
-        mImage        = New<VkSampler2D>( mUIContext->GraphicContext(), lTexture, lSamplingInfo );
-        mHandle       = mUIContext->CreateTextureHandle( mImage );
-        mImagePath    = aImagePath;
-
-        return *this;
-    }
-
-    UIImage &UIImage::SetSize( float aWidth, float aHeight )
-    {
-        mSize = ImVec2{ aWidth, aHeight };
-
-        return *this;
-    }
-
-    UIImage &UIImage::SetBackgroundColor( math::vec4 aColor )
-    {
-        mBackgroundColor = ImVec4{ aColor.x, aColor.y, aColor.z, aColor.w };
-
-        return *this;
-    }
-
-    UIImage &UIImage::SetTintColor( math::vec4 aColor )
-    {
-        mTintColor = ImVec4{ aColor.x, aColor.y, aColor.z, aColor.w };
-
-        return *this;
     }
 
     ImVec2 UIImage::RequiredSize() { return mSize; }
@@ -55,7 +13,7 @@ namespace SE::Core
     {
         ImGui::SetCursorPos( aPosition );
 
-        ImGui::Image( (ImTextureID)mHandle.Handle->GetVkDescriptorSet(), aSize, ImVec2{ 0.0f, 0.0f }, ImVec2{ 1.0f, 1.0f },
+        ImGui::Image( (ImTextureID)mHandle.Handle->GetVkDescriptorSet(), aSize, mTopLeft, mBottomRight,
                       mBackgroundColor, mTintColor );
     }
 
--- Source/UI/Components/Image.h
+++ Source/UI/Components/Image.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Component.h"
+#include "BaseImage.h"
 
 #include "Core/Math/Types.h"
 
@@ -8,33 +8,14 @@
 
 namespace SE::Core
 {
-    class UIImage : public UIComponent
+    class UIImage : public UIBaseImage
     {
       public:
         UIImage() = default;
 
         UIImage( Ref<UIContext> aUIContext, fs::path const &aImagePath, math::vec2 aSize );
 
-        UIImage &SetImage( fs::path const &aImagePath );
-        UIImage &SetSize( float aWidth, float aHeight );
-        UIImage &SetBackgroundColor( math::vec4 aColor );
-        UIImage &SetTintColor( math::vec4 aColor );
-
       private:
-        Ref<UIContext> mUIContext;
-        fs::path       mImagePath;
-
-        Ref<VkSampler2D> mImage;
-        ImageHandle      mHandle;
-
-        ImVec2 mSize{};
-        ImVec4 mBackgroundColor{ 0.0f, 0.0f, 0.0f, 0.0f };
-        ImVec4 mTintColor{ 1.0f, 1.0f, 1.0f, 1.0f };
-
-      private:
-        void PushStyles();
-        void PopStyles();
-
         ImVec2 RequiredSize();
         void   DrawContent( ImVec2 aPosition, ImVec2 aSize );
     };
