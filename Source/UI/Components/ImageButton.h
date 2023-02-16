af50b747d848aca44a1e86d434a47c8c0397cef9
Jean-Martin Albert <jm.albert@gmail.com>
2023-02-16 14:50:08 -0500

    Added configuration functions

--- Source/UI/Components/ImageButton.h
+++ Source/UI/Components/ImageButton.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Component.h"
+
+#include "Core/Math/Types.h"
+
 #include "Core/GraphicContext/UI/UIContext.h"
 
 namespace SE::Core
@@ -10,8 +13,13 @@ namespace SE::Core
       public:
         UIImageButton() = default;
 
-        UIImageButton(  Ref<UIContext> aUIContext, fs::path const &aImagePath );
-        UIImageButton(  Ref<UIContext> aUIContext, fs::path const &aImagePath, std::function<void()> aOnClick );
+        UIImageButton( Ref<UIContext> aUIContext, fs::path const &aImagePath, math::vec2 aSize );
+        UIImageButton( Ref<UIContext> aUIContext, fs::path const &aImagePath, math::vec2 aSize, std::function<void()> aOnClick );
+
+        UIImageButton &SetImage( fs::path const &aImagePath );
+        UIImageButton &SetSize( float aWidth, float aHeignt );
+        UIImageButton &SetBackgroundColor( math::vec4 aColor );
+        UIImageButton &SetTintColor( math::vec4 aColor );
 
       private:
         Ref<UIContext>        mUIContext;
@@ -19,14 +27,15 @@ namespace SE::Core
         std::function<void()> mOnClick;
 
         Ref<VkSampler2D> mImage;
-        ImageHandle      mImageHandle;
+        ImageHandle      mHandle;
+
+        ImVec2 mSize{};
+        ImVec4 mBackgroundColor{ 0.0f, 0.0f, 0.0f, 0.0f };
+        ImVec4 mTintColor{ 1.0f, 1.0f, 1.0f, 1.0f };
 
       private:
         void PushStyles();
         void PopStyles();
-
-        void PushStyles( bool aEnabled );
-        void PopStyles( bool aEnabled );
 
         ImVec2 RequiredSize();
         void   DrawContent( ImVec2 aPosition, ImVec2 aSize );
