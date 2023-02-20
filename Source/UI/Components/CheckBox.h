ed0b7a4e9d51c7698a684de38230a8a19a62d147
Jean-Martin Albert <jm.albert@gmail.com>
2023-02-19 22:46:17 -0500

    Added checkbox

---
+++ Source/UI/Components/CheckBox.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Component.h"
+#include "Label.h"
+
+namespace SE::Core
+{
+    class UICheckBox : public UIComponent
+    {
+      public:
+        UICheckBox() = default;
+        UICheckBox( std::function<void()> aOnClick );
+
+        void OnClick( std::function<void()> aOnClick );
+
+      private:
+        std::function<void()> mOnClick;
+        bool mIsChecked = false;
+
+      private:
+        void PushStyles();
+        void PopStyles();
+
+        void PushStyles( bool aEnabled );
+        void PopStyles( bool aEnabled );
+
+        ImVec2 RequiredSize();
+        void   DrawContent( ImVec2 aPosition, ImVec2 aSize );
+    };
+} // namespace SE::Core
