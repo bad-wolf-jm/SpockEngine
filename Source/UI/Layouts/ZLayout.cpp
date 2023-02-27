47903a93c0940ca96371e45ba35fc926070e3d29
Jean-Martin Albert <jm.albert@gmail.com>
2023-02-27 10:35:38 -0500

    Fixed size now taken into account

--- Source/UI/Layouts/ZLayout.cpp
+++ Source/UI/Layouts/ZLayout.cpp
@@ -2,12 +2,6 @@
 
 namespace SE::Core
 {
-
-    UIZLayout::UIZLayout( eZLayoutOrientation aOrientation )
-        : mOrientation{ aOrientation }
-    {
-    }
-
     void UIZLayout::PushStyles() {}
     void UIZLayout::PopStyles() {}
 
@@ -16,147 +10,145 @@ namespace SE::Core
         float lWidth  = 0.0f;
         float lHeight = 0.0f;
 
-        for( auto const &lItem : mChildren )
-        {
-            ImVec2 lRequiredSize{};
-            if( lItem.mFixedSize > 0.0f )
-            {
-                if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
+        // for( auto const &lItem : mChildren )
+        // {
+        //     ImVec2 lRequiredSize{};
+        //     if( lItem.mFixedSize > 0.0f )
+        //     {
+        //         if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
 
-                if( mOrientation == eZLayoutOrientation::HORIZONTAL )
-                {
-                    lWidth += lItem.mFixedSize;
-                    lHeight = math::max( lHeight, lRequiredSize.y );
-                }
-                else
-                {
-                    lHeight += lItem.mFixedSize;
-                    lWidth = math::max( lWidth, lRequiredSize.x );
-                }
-            }
-            else
-            {
+        //         if( mOrientation == eZLayoutOrientation::HORIZONTAL )
+        //         {
+        //             lWidth += lItem.mFixedSize;
+        //             lHeight = math::max( lHeight, lRequiredSize.y );
+        //         }
+        //         else
+        //         {
+        //             lHeight += lItem.mFixedSize;
+        //             lWidth = math::max( lWidth, lRequiredSize.x );
+        //         }
+        //     }
+        //     else
+        //     {
 
-                if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
+        //         if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
 
-                if( mOrientation == eZLayoutOrientation::HORIZONTAL )
-                {
-                    lWidth += lRequiredSize.x;
-                    lHeight = math::max( lHeight, lRequiredSize.y );
-                }
-                else
-                {
-                    lHeight += lRequiredSize.y;
-                    lWidth = math::max( lWidth, lRequiredSize.x );
-                }
-            }
-        }
+        //         if( mOrientation == eZLayoutOrientation::HORIZONTAL )
+        //         {
+        //             lWidth += lRequiredSize.x;
+        //             lHeight = math::max( lHeight, lRequiredSize.y );
+        //         }
+        //         else
+        //         {
+        //             lHeight += lRequiredSize.y;
+        //             lWidth = math::max( lWidth, lRequiredSize.x );
+        //         }
+        //     }
+        // }
 
         return ImVec2{ lWidth, lHeight };
     }
 
-    void UIZLayout::SetItemSpacing( float aItemSpacing ) { mItemSpacing = aItemSpacing; }
-
     void UIZLayout::Add( UIComponent *aChild, bool aExpand, bool aFill )
     {
-        Add( aChild, 0.0f, aExpand, aFill, eHorizontalAlignment::CENTER, eVerticalAlignment::CENTER );
+        Add( aChild, math::vec2{}, aExpand, aFill, eHorizontalAlignment::CENTER, eVerticalAlignment::CENTER );
     }
 
     void UIZLayout::Add( UIComponent *aChild, bool aExpand, bool aFill, eHorizontalAlignment const &aHAlignment,
                          eVerticalAlignment const &aVAlignment )
     {
-        Add( aChild, 0.0f, aExpand, aFill, aHAlignment, aVAlignment );
+        Add( aChild, math::vec2{}, aExpand, aFill, aHAlignment, aVAlignment );
     }
 
-    void UIZLayout::Add( UIComponent *aChild, float aFixedSize, bool aExpand, bool aFill )
+    void UIZLayout::Add( UIComponent *aChild, math::vec2 aFixedSize, bool aExpand, bool aFill )
     {
-        Add( aChild, 0.0f, aExpand, aFill, eHorizontalAlignment::CENTER, eVerticalAlignment::CENTER );
+        Add( aChild, aFixedSize, aExpand, aFill, eHorizontalAlignment::CENTER, eVerticalAlignment::CENTER );
     }
 
-    void UIZLayout::Add( UIComponent *aChild, float aFixedSize, bool aExpand, bool aFill, eHorizontalAlignment const &aHAlignment,
+    void UIZLayout::Add( UIComponent *aChild, math::vec2 aFixedSize, bool aExpand, bool aFill, eHorizontalAlignment const &aHAlignment,
                          eVerticalAlignment const &aVAlignment )
     {
-        mChildren.push_back( ZLayoutItem{ aChild, 0.0f, aExpand, aFill, aHAlignment, aVAlignment } );
+        mChildren.push_back( ZLayoutItem{ aChild, aFixedSize, aExpand, aFill, aHAlignment, aVAlignment } );
     }
 
     void UIZLayout::DrawContent( ImVec2 aPosition, ImVec2 aSize )
     {
-        uint32_t lExpandCount = 0;
-        float    lTaken       = mItemSpacing * ( mChildren.size() - 1 );
+        // uint32_t lExpandCount = 0;
+        // float    lTaken       = mItemSpacing * ( mChildren.size() - 1 );
 
-        for( auto const &lItem : mChildren )
-        {
-            lExpandCount += lItem.mExpand ? 1 : 0;
+        // for( auto const &lItem : mChildren )
+        // {
+        //     lExpandCount += lItem.mExpand ? 1 : 0;
 
-            if( lItem.mFixedSize > 0.0f )
-            {
-                lTaken += lItem.mFixedSize;
-            }
-            else if( !lItem.mExpand )
-            {
-                auto lRequiredSize = lItem.mItem->RequiredSize();
+        //     if( lItem.mFixedSize > 0.0f )
+        //     {
+        //         lTaken += lItem.mFixedSize;
+        //     }
+        //     else if( !lItem.mExpand )
+        //     {
+        //         auto lRequiredSize = lItem.mItem->RequiredSize();
 
-                lTaken += ( mOrientation == eZLayoutOrientation::HORIZONTAL ) ? lRequiredSize.x : lRequiredSize.y;
-            }
-        }
+        //         lTaken += ( mOrientation == eZLayoutOrientation::HORIZONTAL ) ? lRequiredSize.x : lRequiredSize.y;
+        //     }
+        // }
 
-        ImVec2 lExpandedSize{};
+        // ImVec2 lExpandedSize{};
 
-        if( mOrientation == eZLayoutOrientation::HORIZONTAL )
-            lExpandedSize = ImVec2{ ( aSize.x - lTaken ) / lExpandCount, aSize.y };
-        else
-            lExpandedSize = ImVec2{ aSize.x, ( aSize.y - lTaken ) / lExpandCount };
+        // if( mOrientation == eZLayoutOrientation::HORIZONTAL )
+        //     lExpandedSize = ImVec2{ ( aSize.x - lTaken ) / lExpandCount, aSize.y };
+        // else
+        //     lExpandedSize = ImVec2{ aSize.x, ( aSize.y - lTaken ) / lExpandCount };
 
-        ImVec2 lCurrentPosition = aPosition;
-        for( auto const &lItem : mChildren )
-        {
-            ImVec2 lItemSize{};
-            ImVec2 lItemPosition{};
-            float  lPositionStep = 0.0f;
+        // ImVec2 lCurrentPosition = aPosition;
+        // for( auto const &lItem : mChildren )
+        // {
+        //     ImVec2 lItemSize{};
+        //     ImVec2 lItemPosition{};
+        //     float  lPositionStep = 0.0f;
 
-            if( lItem.mExpand )
-            {
-                lItemSize     = lItem.mFill ? lExpandedSize : lItem.mItem->RequiredSize();
-                lItemPosition = lItem.mFill ? lCurrentPosition
-                                            : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition, lItemSize,
-                                                                         lExpandedSize );
-                lPositionStep = ( mOrientation == eZLayoutOrientation::VERTICAL ) ? lExpandedSize.y : lExpandedSize.x;
-            }
-            else if( lItem.mFixedSize > 0.0f )
-            {
-                if( mOrientation == eZLayoutOrientation::VERTICAL )
-                    lItemSize = ImVec2{ lItem.mFixedSize, aSize.y };
-                else
-                    lItemSize = ImVec2{ aSize.x, lItem.mFixedSize };
+        //     if( lItem.mExpand )
+        //     {
+        //         lItemSize     = lItem.mFill ? lExpandedSize : lItem.mItem->RequiredSize();
+        //         lItemPosition = lItem.mFill ? lCurrentPosition
+        //                                     : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition, lItemSize,
+        //                                                                  lExpandedSize );
+        //         lPositionStep = ( mOrientation == eZLayoutOrientation::VERTICAL ) ? lExpandedSize.y : lExpandedSize.x;
+        //     }
+        //     else if( lItem.mFixedSize > 0.0f )
+        //     {
+        //         if( mOrientation == eZLayoutOrientation::VERTICAL )
+        //             lItemSize = ImVec2{ lItem.mFixedSize, aSize.y };
+        //         else
+        //             lItemSize = ImVec2{ aSize.x, lItem.mFixedSize };
 
-                lItemSize     = lItem.mFill ? lItemSize : lItem.mItem->RequiredSize();
-                lItemPosition = lItem.mFill ? lCurrentPosition
-                                            : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
-                                                                         lItem.mItem->RequiredSize(), lItemSize );
-                lPositionStep = lItem.mFixedSize;
-            }
-            else
-            {
-                lItemSize = lItem.mItem->RequiredSize();
+        //         lItemSize     = lItem.mFill ? lItemSize : lItem.mItem->RequiredSize();
+        //         lItemPosition = lItem.mFill ? lCurrentPosition
+        //                                     : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
+        //                                                                  lItem.mItem->RequiredSize(), lItemSize );
+        //         lPositionStep = lItem.mFixedSize;
+        //     }
+        //     else
+        //     {
+        //         lItemSize = lItem.mItem->RequiredSize();
 
-                if( mOrientation == eZLayoutOrientation::VERTICAL )
-                    lItemSize.x = aSize.x;
-                else
-                    lItemSize.y = aSize.y;
+        //         if( mOrientation == eZLayoutOrientation::VERTICAL )
+        //             lItemSize.x = aSize.x;
+        //         else
+        //             lItemSize.y = aSize.y;
 
-                lItemSize     = lItem.mFill ? lItemSize : lItem.mItem->RequiredSize();
-                lItemPosition = lItem.mFill ? lCurrentPosition
-                                            : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
-                                                                         lItem.mItem->RequiredSize(), lItemSize );
-                lPositionStep = ( mOrientation == eZLayoutOrientation::VERTICAL ) ? lItemSize.y : lItemSize.x;
-            }
+        //         lItemSize     = lItem.mFill ? lItemSize : lItem.mItem->RequiredSize();
+        //         lItemPosition = lItem.mFill ? lCurrentPosition
+        //                                     : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
+        //                                                                  lItem.mItem->RequiredSize(), lItemSize );
+        //         lPositionStep = ( mOrientation == eZLayoutOrientation::VERTICAL ) ? lItemSize.y : lItemSize.x;
+        //     }
 
-            if( lItem.mItem ) lItem.mItem->Update( lItemPosition, lItemSize );
+        //     if( lItem.mItem ) lItem.mItem->Update( lItemPosition, lItemSize );
 
-            if( mOrientation == eZLayoutOrientation::VERTICAL )
-                lCurrentPosition.y += ( lPositionStep + mItemSpacing );
-            else
-                lCurrentPosition.x += ( lPositionStep + mItemSpacing );
-        }
+        //     if( mOrientation == eZLayoutOrientation::VERTICAL )
+        //         lCurrentPosition.y += ( lPositionStep + mItemSpacing );
+        //     else
+        //         lCurrentPosition.x += ( lPositionStep + mItemSpacing );
+        // }
     }
 } // namespace SE::Core
