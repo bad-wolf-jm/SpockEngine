87c843b5492ab86709d96c81240cace14ff93e75
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-06 19:10:28 -0500

    Fleshed out table definition

--- Source/UI/Components/Table.h
+++ Source/UI/Components/Table.h
@@ -4,6 +4,21 @@
 
 namespace SE::Core
 {
+    struct sTableColumn
+    {
+        std::string mHeader;
+        float       mInitialSize = 10.0f;
+
+        virtual void Render( void *aDataToRender, ImVec2 aCellSize ) = 0;
+    };
+
+    struct sTableData
+    {
+        virtual int CountRows() = 0;
+
+        virtual void *Get( int row, int column ) = 0;
+    };
+
     class UITable : public UIComponent
     {
       public:
@@ -11,10 +26,16 @@ namespace SE::Core
 
         UITable( std::string const &aText );
 
-        void SetText( std::string const &aText );
+        void AddColumn( Ref<sTableColumn> aColumn );
+        void SetData( Ref<sTableData> aData );
+        void SetRowHeight( float aRowHeight );
 
       protected:
-        std::string mText;
+        std::vector<Ref<sTableColumn>> mColumns;
+        Ref<sTableData>                mData;
+
+      protected:
+        float mRowHeight = 15.0f;
 
       protected:
         void PushStyles();
