28270a4f209c8042ee6c0ae1c26d6d0f9b06446c
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-07 16:55:21 -0500

    SetData function for link element table

--- Source/OtdrEditor/LinkElementTable.cpp
+++ Source/OtdrEditor/LinkElementTable.cpp
@@ -14,7 +14,8 @@ namespace SE::OtdrEditor
     using namespace SE::Core;
     // using namespace SE::Core::EntityComponentSystem::Components;
 
-    UILinkElementTable::UILinkElementTable() : UITable()
+    UILinkElementTable::UILinkElementTable()
+        : UITable()
     {
         SetRowHeight( 20.0f );
 
@@ -68,9 +69,74 @@ namespace SE::OtdrEditor
         AddColumn( mEventType );
 
         mEventStatus = New<sFloat64Column>( "mEventStatus", 75.0f, "{:.3f} dB/km", "\xe2\x88\x9e km" );
-        AddColumn(mEventStatus);
+        AddColumn( mEventStatus );
 
         mReflectanceType = New<sFloat64Column>( "mReflectanceType", 75.0f, "{:.3f} dB/km", "\xe2\x88\x9e km" );
-        AddColumn(mReflectanceType);
+        AddColumn( mReflectanceType );
     }
+
+    void UILinkElementTable::SetData( std::vector<sLinkElement> const &aData )
+    {
+        mEventDataVector = std::move( aData );
+
+        mType->mData.clear();
+        for( auto const &lE : mEventDataVector ) mType->mData.push_back( 0.0f );
+
+        mStatus->mData.clear();
+        for( auto const &lE : mEventDataVector ) mStatus->mData.push_back( 0.0f );
+
+        mDiagnosicCount->mData.clear();
+        for( auto const &lE : mEventDataVector ) mDiagnosicCount->mData.push_back( 0.0f );
+
+        mPositionColumn->mData.clear();
+        for( auto const &lE : mEventDataVector ) mPositionColumn->mData.push_back( lE.mPosition * 0.001f );
+
+        mPreviousFiberSectionA->mData.clear();
+        for( auto const &lE : mEventDataVector ) mPreviousFiberSectionA->mData.push_back( lE.mPreviousFiberSectionA * 0.001f );
+
+        mPreviousFiberSectionB->mData.clear();
+        for( auto const &lE : mEventDataVector ) mPreviousFiberSectionB->mData.push_back( lE.mPreviousFiberSectionB * 0.001f );
+
+        mPreviousFiberSectionLength->mData.clear();
+        for( auto const &lE : mEventDataVector )
+            mPreviousFiberSectionLength->mData.push_back( lE.mPreviousFiberSectionLength * 0.001f );
+
+        mPreviousFiberSectionLoss->mData.clear();
+        for( auto const &lE : mEventDataVector ) mPreviousFiberSectionLoss->mData.push_back( lE.mPreviousFiberSectionLoss * 0.001f );
+
+        mPreviousFiberSectionAttenuation->mData.clear();
+        for( auto const &lE : mEventDataVector )
+            mPreviousFiberSectionAttenuation->mData.push_back( lE.mPreviousFiberSectionAttenuation * 0.001f );
+
+        mLoss->mData.clear();
+        for( auto const &lE : mEventDataVector ) mLoss->mData.push_back( lE.mLoss * 0.001f );
+
+        mLossPassFail->mData.clear();
+        for( auto const &lE : mEventDataVector ) mLossPassFail->mData.push_back( 0.0f );
+
+        mLossError->mData.clear();
+        for( auto const &lE : mEventDataVector ) mLossError->mData.push_back( 0.0f );
+
+        mReflectance->mData.clear();
+        for( auto const &lE : mEventDataVector ) mReflectance->mData.push_back( lE.mReflectance );
+
+        mReflectancePassFail->mData.clear();
+        for( auto const &lE : mEventDataVector ) mReflectancePassFail->mData.push_back( 0.0f );
+
+        mCurveLevelColumn->mData.clear();
+        for( auto const &lE : mEventDataVector ) mCurveLevelColumn->mData.push_back( lE.mCurveLevel );
+
+        mPositionTolerance->mData.clear();
+        for( auto const &lE : mEventDataVector ) mPositionTolerance->mData.push_back( lE.mPositionTolerance );
+
+        mEventType->mData.clear();
+        for( auto const &lE : mEventDataVector ) mEventType->mData.push_back( 0.0f );
+
+        mEventStatus->mData.clear();
+        for( auto const &lE : mEventDataVector ) mEventStatus->mData.push_back( 0.0f );
+
+        mReflectanceType->mData.clear();
+        for( auto const &lE : mEventDataVector ) mReflectanceType->mData.push_back( 0.0f );
+    }
+
 } // namespace SE::OtdrEditor
