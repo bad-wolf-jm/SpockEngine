8e49adc07bb30c5e1bab61ea4b3c63742e0f0a9a
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-06 19:10:28 -0500

    Fleshed out table definition

--- Source/UI/Components/Table.cpp
+++ Source/UI/Components/Table.cpp
@@ -3,28 +3,57 @@
 namespace SE::Core
 {
 
-    UITable::UITable( std::string const &aText )
-        : mText{ aText }
-    {
-    }
-
     void UITable::PushStyles() {}
     void UITable::PopStyles() {}
 
-    void UITable::SetText( std::string const &aText ) { mText = aText; }
+    void UITable::SetRowHeight( float aRowHeight ) { mRowHeight = aRowHeight; }
 
-    ImVec2 UITable::RequiredSize()
-    {
-        auto lTextSize = ImGui::CalcTextSize( mText.c_str() );
+    void UITable::AddColumn( Ref<sTableColumn> aColumn ) { mColumns.push_back( aColumn ); }
 
-        return lTextSize;
-    }
+    void UITable::SetData( Ref<sTableData> aData ) { mData = aData; }
+
+    ImVec2 UITable::RequiredSize() { return ImVec2{}; }
 
     void UITable::DrawContent( ImVec2 aPosition, ImVec2 aSize )
     {
-        ImGui::SetCursorPos( GetContentAlignedposition(mHAlign, mVAlign, aPosition, ImGui::CalcTextSize( mText.c_str() ), aSize) );
+        ImGui::SetCursorPos( aPosition );
 
-        ImGui::Text( mText.c_str(), aSize );
+        const ImGuiTableFlags lTableFlags = ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_ScrollX | ImGuiTableFlags_ScrollY |
+                                            ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_Resizable |
+                                            ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable;
+
+        if( ImGui::BeginTable( "##", mColumns.size(), lTableFlags, aSize ) )
+        {
+            ImGuiTable *lThisTable = ImGui::GetCurrentContext()->CurrentTable;
+
+            for( const auto &lColumn : mColumns )
+                ImGui::TableSetupColumn( lColumn->mHeader.c_str(), ImGuiTableColumnFlags_None, lColumn->mInitialSize );
+
+            ImGui::TableHeadersRow();
+
+            ImGuiListClipper lRowClipping;
+            lRowClipping.Begin( mData->CountRows() );
+            ImGui::TableNextRow();
+            while( lRowClipping.Step() )
+            {
+                for( int lRow = lRowClipping.DisplayStart; lRow < lRowClipping.DisplayEnd; lRow++ )
+                {
+                    ImGui::TableNextRow();
+
+                    int lColumn = 0;
+                    for( const auto &lColumnData : mColumns )
+                    {
+                        ImGui::TableSetColumnIndex( lColumn );
+
+                        float lWidth  = lThisTable->Columns[lColumn].ItemWidth;
+                        float lHeight = 30.0f;
+
+                        lColumnData->Render( mData->Get( lRow, lColumn ), ImVec2{ lWidth, lHeight } );
+                    }
+                }
+            }
+            ImGui::EndTable();
+        }
     }
 
 } // namespace SE::Core
