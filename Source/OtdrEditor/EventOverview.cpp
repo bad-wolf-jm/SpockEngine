2d77cb2511610f1c087d9f12e1ca0f137211c7d9
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-10 11:29:34 -0500

    Class structure for event property sheet

--- Source/OtdrEditor/EventOverview.cpp
+++ Source/OtdrEditor/EventOverview.cpp
@@ -2,83 +2,124 @@
 
 namespace SE::OtdrEditor
 {
+    EventOverview::EventOverview()
+        : UIBoxLayout( eBoxLayoutOrientation::VERTICAL )
+    {
 
-}
+        const float lItemHeight = 20.0f;
 
-// <html><pre><font color=#342826 face="courrier" size="3"><b>Meas. results</b></font></pre></html>	
-//  Overv. length	11.656 km
-//  Link length	9.2936 km
-// <html><pre><font color=#342826 face="courrier" size="3"><b>   � 1550nm</b></font></pre></html>	
-//      Loss 	18.65 dB
-//      ORL 	54.71 dB
-//      Ov Length 	11.665 km
-//      Ov Loss 	19.66 dB
-//      Propag delay	45.52 µs
-// <html><pre><font color=#342826 face="courrier" size="3"><b>   � 1310nm</b></font></pre></html>	
-//      Loss 	20.56 dB
-//      ORL 	56.16 dB
-//      Ov Length 	11.672 km
-//      Ov Loss 	21.82 dB
-//      Propag delay	45.48 µs
-// <html><pre><font color=#342826 face="courrier" size="3"><b>   � 1625nm</b></font></pre></html>	
-//      Loss 	18.78 dB
-//      ORL 	54.39 dB
-//      Ov Length 	11.656 km
-//      Ov Loss 	19.56 dB
-//      Propag delay	45.50 µs
-// Completion status	Completed
-// <html><pre><font color=#342826 face="courrier" size="3"><b>Configuration</b></font></pre></html>	
-// Launch fiber	159.75 m     [ 15 m-  5 km ]
-// Conditioner	None
-// Receive fiber	2.20 km     [  2 km-10.0 km @22dB ]
-// Loop length	--- m     [--- m-5.0 km ]
-// Fiber code	B
-// IOR @1550nm	1.468325
-// RBS @1550nm	-81.87 dB
-// Att @1550nm	0.20 dB/km
-// Wavelengths	1310nm 1550nm 1625nm 
-// Direction	AB
-// Loopback extract	No
-// Expected elements	2
-//    Splitter	1x8
-//    Splitter	1x4
-// Topology	None
-// Measurement type	Not set
-// Sequence type	Not set
-// Macbend threshold	0.5 dB
-// Comment	
-// Advisor mode	No
-// Duration	00:03:07.1839285
-// Date	2022-05-30 10:15:00 AM
-// <html><pre><font color=#342826 face="courrier" size="3"><b>Analysis results</b></font></pre></html>	
-// EOL link loss	NaN dB
-// EOL position	--- m
-// Proc. time	--- s
-// <html><pre><font color=#342826 face="courrier" size="3"><b>Test fibers</b></font></pre></html>	0 items
-// <html><pre><font color=#342826 face="courrier" size="3"><b>Software versions</b></font></pre></html>	(Metrino)
-// Olm.Instrument	1.2.22026.2
-// Otdr.Instrument	???
-// Olm.SignalProcessing	1.22.22026.2
-// Otdr.SignalProcessing	???
-// Otdr.Simulation	???
-// llKit:iOLM	1.22.22026.2
-// <html><pre><font color=#342826 face="courrier" size="3"><b>Hardware</b></font></pre></html>	
-// Model name	MAX-730C-SM3-EA
-// S/N	1465558
-// OTDR Family	F
-// Model type	
-// Product version	C
-// Power level	Regular
-// Fiber code	B - 9�m
-// Output ports	1
-// FPGA version	PL294 1.3.0.0
-// PCB version	B
-// PCB s/n	1465558
-// CPUPCB version	
-// CPUPCB s/n	
-// PM version	
-// PM sn	
-// Manuf. date	2021-05-19 12:00:00 AM
-// Last calib.	2021-12-21 12:00:00 AM
-// User last calib.	2021-12-21 12:00:00 AM
-// EEPROM data	Empty
+        mEventOverview = New<UILabel>( "Event properties" );
+        mEventOverview->SetAlignment( eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+        Add( mEventOverview.get(), 30.0f, false, true, eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+        mEventOverviewLayout         = New<UIBoxLayout>( eBoxLayoutOrientation::HORIZONTAL );
+        mEventOverviewPropertyLayout = New<UIBoxLayout>( eBoxLayoutOrientation::VERTICAL );
+        mEventOverviewLayout->Add( nullptr, 25.0f, false, false );
+        mEventOverviewLayout->Add( mEventOverviewPropertyLayout.get(), true, true );
+        mWavelength = New<UIPropertyValue>( "Overview length:" );
+        mEventOverviewPropertyLayout->Add( mWavelength.get(), lItemHeight, false, true );
+        mDetectionTrace = New<UIPropertyValue>( "Link length:" );
+        mEventOverviewPropertyLayout->Add( mDetectionTrace.get(), lItemHeight, false, true );
+        mPositionTolerance = New<UIPropertyValue>( "Completion:" );
+        mEventOverviewPropertyLayout->Add( mPositionTolerance.get(), lItemHeight, false, true );
+        mCurveLevel = New<UIPropertyValue>( "Overview length:" );
+        mEventOverviewPropertyLayout->Add( mCurveLevel.get(), lItemHeight, false, true );
+        mEndOrNoiseLevel = New<UIPropertyValue>( "Link length:" );
+        mEventOverviewPropertyLayout->Add( mEndOrNoiseLevel.get(), lItemHeight, false, true );
+        mEstimatedCurveLevel = New<UIPropertyValue>( "Completion:" );
+        mEventOverviewPropertyLayout->Add( mEstimatedCurveLevel.get(), lItemHeight, false, true );
+        mEstimatedEndLevel = New<UIPropertyValue>( "Overview length:" );
+        mEventOverviewPropertyLayout->Add( mEstimatedEndLevel.get(), lItemHeight, false, true );
+        mEstimatedLoss = New<UIPropertyValue>( "Link length:" );
+        mEventOverviewPropertyLayout->Add( mEstimatedLoss.get(), lItemHeight, false, true );
+        mPeakSNR = New<UIPropertyValue>( "Completion:" );
+        mEventOverviewPropertyLayout->Add( mPeakSNR.get(), lItemHeight, false, true );
+        mPeakTrace = New<UIPropertyValue>( "Overview length:" );
+        mEventOverviewPropertyLayout->Add( mPeakTrace.get(), lItemHeight, false, true );
+        mPeakPosition = New<UIPropertyValue>( "Link length:" );
+        mEventOverviewPropertyLayout->Add( mPeakPosition.get(), lItemHeight, false, true );
+        mPeakPower = New<UIPropertyValue>( "Completion:" );
+        mEventOverviewPropertyLayout->Add( mPeakPower.get(), lItemHeight, false, true );
+        mPeakSaturation = New<UIPropertyValue>( "Overview length:" );
+        mEventOverviewPropertyLayout->Add( mPeakSaturation.get(), lItemHeight, false, true );
+        mFresnelCorrection = New<UIPropertyValue>( "Link length:" );
+        mEventOverviewPropertyLayout->Add( mFresnelCorrection.get(), lItemHeight, false, true );
+        mUseSinglePulse = New<UIPropertyValue>( "Completion:" );
+        mEventOverviewPropertyLayout->Add( mUseSinglePulse.get(), lItemHeight, false, true );
+        Add( mEventOverviewLayout.get(), false, true, eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+
+        mPreviousSectionTitle = New<UILabel>( "Previous section" );
+        mPreviousSectionTitle->SetAlignment( eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+        Add( mPreviousSectionTitle.get(), 30.0f, false, true, eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+        mPreviousSectionLayout         = New<UIBoxLayout>( eBoxLayoutOrientation::HORIZONTAL );
+        mPreviousSectionPropertyLayout = New<UIBoxLayout>( eBoxLayoutOrientation::VERTICAL );
+        mPreviousSectionLayout->Add( nullptr, 25.0f, false, false );
+        mPreviousSectionLayout->Add( mPreviousSectionPropertyLayout.get(), true, true );
+        mPreviousRbsTrace = New<UIPropertyValue>( "Trace:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousRbsTrace.get(), lItemHeight, false, true );
+        mPreviousRbsNoise = New<UIPropertyValue>( "Noise:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousRbsNoise.get(), lItemHeight, false, true );
+        mPreviousRbsSaturation = New<UIPropertyValue>( "Saturation:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousRbsSaturation.get(), lItemHeight, false, true );
+        mPreviousLsaRange = New<UIPropertyValue>( "LSA range:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaRange.get(), lItemHeight, false, true );
+        mPreviousLsaSlope = New<UIPropertyValue>( "Lsa slope:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaSlope.get(), lItemHeight, false, true );
+        mPreviousLsaOffset = New<UIPropertyValue>( "Lsa offset:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaOffset.get(), lItemHeight, false, true );
+        mPreviousLsaMean = New<UIPropertyValue>( "lsa mean:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaMean.get(), lItemHeight, false, true );
+        mPreviousLsaSlopeError = New<UIPropertyValue>( "Lsa slope error:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaSlopeError.get(), lItemHeight, false, true );
+        mPreviousLsaLinear = New<UIPropertyValue>( "LSA linear:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaLinear.get(), lItemHeight, false, true );
+        mPreviousLsaError = New<UIPropertyValue>( "LSA error:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaError.get(), lItemHeight, false, true );
+        mPreviousLsaCrossings = New<UIPropertyValue>( "LSA crossings:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaCrossings.get(), lItemHeight, false, true );
+        mPreviousLsaSections = New<UIPropertyValue>( "LSA sections:" );
+        mPreviousSectionPropertyLayout->Add( mPreviousLsaSections.get(), lItemHeight, false, true );
+        Add( mPreviousSectionLayout.get(), false, true, eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+
+        mNextSectionTitle = New<UILabel>( "Previous section" );
+        mNextSectionTitle->SetAlignment( eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+        Add( mNextSectionTitle.get(), 30.0f, false, true, eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+        mNextSectionLayout         = New<UIBoxLayout>( eBoxLayoutOrientation::HORIZONTAL );
+        mNextSectionPropertyLayout = New<UIBoxLayout>( eBoxLayoutOrientation::VERTICAL );
+        mNextSectionLayout->Add( nullptr, 25.0f, false, false );
+        mNextSectionLayout->Add( mNextSectionPropertyLayout.get(), true, true );
+        mNextRbsTrace = New<UIPropertyValue>( "Trace:" );
+        mNextSectionPropertyLayout->Add( mPreviousRbsTrace.get(), lItemHeight, false, true );
+        mNextRbsNoise = New<UIPropertyValue>( "Noise:" );
+        mNextSectionPropertyLayout->Add( mPreviousRbsNoise.get(), lItemHeight, false, true );
+        mNextRbsSaturation = New<UIPropertyValue>( "Saturation:" );
+        mNextSectionPropertyLayout->Add( mPreviousRbsSaturation.get(), lItemHeight, false, true );
+        mNextLsaRange = New<UIPropertyValue>( "LSA range:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaRange.get(), lItemHeight, false, true );
+        mNextLsaSlope = New<UIPropertyValue>( "Lsa slope:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaSlope.get(), lItemHeight, false, true );
+        mNextLsaOffset = New<UIPropertyValue>( "Lsa offset:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaOffset.get(), lItemHeight, false, true );
+        mNextLsaMean = New<UIPropertyValue>( "lsa mean:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaMean.get(), lItemHeight, false, true );
+        mNextLsaSlopeError = New<UIPropertyValue>( "Lsa slope error:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaSlopeError.get(), lItemHeight, false, true );
+        mNextLsaLinear = New<UIPropertyValue>( "LSA linear:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaLinear.get(), lItemHeight, false, true );
+        mNextLsaError = New<UIPropertyValue>( "LSA error:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaError.get(), lItemHeight, false, true );
+        mNextLsaCrossings = New<UIPropertyValue>( "LSA crossings:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaCrossings.get(), lItemHeight, false, true );
+        mNextLsaSections = New<UIPropertyValue>( "LSA sections:" );
+        mNextSectionPropertyLayout->Add( mPreviousLsaSections.get(), lItemHeight, false, true );
+        Add( mNextSectionLayout.get(), true, true, eHorizontalAlignment::LEFT, eVerticalAlignment::CENTER );
+    }
+
+    void EventOverview::SetData( Ref<MonoScriptInstance> aEventOverview )
+    {
+        // Overview
+
+        // Previous section
+
+        // Next section
+    }
+} // namespace SE::OtdrEditor
