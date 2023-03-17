f9b551228e4c94eddd41f9458d5c050df47707ab
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-17 12:39:29 -0400

    Set text color and background color for labels

--- Source/UI/Components/Label.h
+++ Source/UI/Components/Label.h
@@ -12,9 +12,13 @@ namespace SE::Core
         UILabel( std::string const &aText );
 
         void SetText( std::string const &aText );
+        void SetTextColor( math::vec4 aColor );
+        void SetBackgroundColor( math::vec4 aColor );
 
       protected:
         std::string mText;
+        ImVec4      mBackgroundColor;
+        ImVec4      mTextColor;
 
       protected:
         void PushStyles();
