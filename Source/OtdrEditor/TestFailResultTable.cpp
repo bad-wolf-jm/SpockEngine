ec3bc4455d8fcb28e3ad09d5a94aa0874c8514ea
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-20 16:09:16 -0400

    Removed unused columns

--- Source/OtdrEditor/TestFailResultTable.cpp
+++ Source/OtdrEditor/TestFailResultTable.cpp
@@ -19,42 +19,42 @@ namespace SE::OtdrEditor
     {
         SetRowHeight( 20.0f );
 
-        mTestName = New<sStringColumn>( "mTestName", 75.0f );
+        mTestName = New<sStringColumn>( "Test", 75.0f );
         AddColumn( mTestName );
 
-        mTestDate = New<sStringColumn>( "mTestDate", 75.0f );
+        mTestDate = New<sStringColumn>( "Date", 75.0f );
         AddColumn( mTestDate );
  
-        mFilename = New<sStringColumn>( "mFilename", 75.0f );
+        mFilename = New<sStringColumn>( "File", 75.0f );
         AddColumn( mFilename );
 
-        mMessage = New<sStringColumn>( "mMessage", 75.0f );
+        mMessage = New<sStringColumn>( "Message", 75.0f );
         AddColumn( mMessage );
 
         mPhysicalEventPosition = New<sFloat64Column>( "mPhysicalEventPosition", 75.0f, "{:.4f} km", "N.a.N.");
         AddColumn( mPhysicalEventPosition );
 
-        mLinkElementIndex = New<sStringColumn>( "mLinkElementIndex", 75.0f );
-        AddColumn( mLinkElementIndex );
+        // mLinkElementIndex = New<sStringColumn>( "mLinkElementIndex", 75.0f );
+        // AddColumn( mLinkElementIndex );
 
-        mSubLinkElementIndex = New<sStringColumn>( "mSubLinkElementIndex", 75.0f );
-        AddColumn( mSubLinkElementIndex );
+        // mSubLinkElementIndex = New<sStringColumn>( "mSubLinkElementIndex", 75.0f );
+        // AddColumn( mSubLinkElementIndex );
 
-        mPhysicalEventIndex = New<sStringColumn>( "mPhysicalEventIndex", 75.0f );
-        AddColumn( mPhysicalEventIndex );
+        // mPhysicalEventIndex = New<sStringColumn>( "mPhysicalEventIndex", 75.0f );
+        // AddColumn( mPhysicalEventIndex );
 
         mLinkElementPosition = New<sFloat64Column>( "mLinkElementPosition", 75.0f, "{:.4f} km", "N.a.N." );
         AddColumn( mLinkElementPosition );
 
-        mIsSubElement = New<sStringColumn>( "mIsSubElement", 75.0f );
-        AddColumn( mIsSubElement );
+        // mIsSubElement = New<sStringColumn>( "mIsSubElement", 75.0f );
+        // AddColumn( mIsSubElement );
 
         mWavelength = New<sFloat64Column>( "mWavelength", 75.0f, "{:.1f} nm", "N.a.N." );
         AddColumn( mWavelength );
 
 
-        mSinglePulseTraceIndex = New<sStringColumn>( "mSinglePulseTraceIndex", 75.0f );
-        AddColumn( mSinglePulseTraceIndex );
+        // mSinglePulseTraceIndex = New<sStringColumn>( "mSinglePulseTraceIndex", 75.0f );
+        // AddColumn( mSinglePulseTraceIndex );
 
         UITable::OnRowClicked(
             [&]( uint32_t aRow )
@@ -80,14 +80,14 @@ namespace SE::OtdrEditor
             mTestDate->mData.push_back( lDataRow.mTestDate );
 
             mFilename->mData.push_back( fs::path(lDataRow.mFilename).filename().string() );
-            mLinkElementIndex->mData.push_back( lDataRow.mLinkElementIndex );
-            mSubLinkElementIndex->mData.push_back( lDataRow.mSubLinkElementIndex );
-            mPhysicalEventIndex->mData.push_back( lDataRow.mPhysicalEventIndex );
+            // mLinkElementIndex->mData.push_back( lDataRow.mLinkElementIndex );
+            // mSubLinkElementIndex->mData.push_back( lDataRow.mSubLinkElementIndex );
+            // mPhysicalEventIndex->mData.push_back( lDataRow.mPhysicalEventIndex );
             mLinkElementPosition->mData.push_back( lDataRow.mLinkElementPosition * 0.001f );
-            mIsSubElement->mData.push_back( lDataRow.mIsSubElement );
+            // mIsSubElement->mData.push_back( lDataRow.mIsSubElement );
             mWavelength->mData.push_back( lDataRow.mWavelength * 1e9 );
             mPhysicalEventPosition->mData.push_back( lDataRow.mPhysicalEventPosition * 0.001f );
-            mSinglePulseTraceIndex->mData.push_back( lDataRow.mSinglePulseTraceIndex );
+            // mSinglePulseTraceIndex->mData.push_back( lDataRow.mSinglePulseTraceIndex );
             mMessage->mData.push_back( lDataRow.mMessage );
         }
     }
@@ -98,14 +98,14 @@ namespace SE::OtdrEditor
         mTestName->mData.clear();
         mTestDate->mData.clear();
         mFilename->mData.clear();
-        mLinkElementIndex->mData.clear();
-        mSubLinkElementIndex->mData.clear();
-        mPhysicalEventIndex->mData.clear();
+        // mLinkElementIndex->mData.clear();
+        // mSubLinkElementIndex->mData.clear();
+        // mPhysicalEventIndex->mData.clear();
         mLinkElementPosition->mData.clear();
-        mIsSubElement->mData.clear();
+        // mIsSubElement->mData.clear();
         mWavelength->mData.clear();
         mPhysicalEventPosition->mData.clear();
-        mSinglePulseTraceIndex->mData.clear();
+        // mSinglePulseTraceIndex->mData.clear();
         mMessage->mData.clear();
     }
 
