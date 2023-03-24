529778143b5d83266883ae918aef8722429f32cb
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-24 18:36:34 -0400

    Tests

--- Programs/TestOtdrProject/Source/Application.cs
+++ Programs/TestOtdrProject/Source/Application.cs
@@ -16,7 +16,9 @@ namespace Test
     {
         UIForm mMainForm;
         UILabel mLabel0;
+        UIButton mButton0;
 
+        UIBoxLayout mLayout0;
         public App() { }
 
         public void Initialize()
@@ -25,22 +27,25 @@ namespace Test
             mLabel0 = new UILabel("Test label");
             mLabel0.SetTextColor(new vec4(1.0f, 0.0f, 1.0f, 1.0f));
 
+            mButton0 = new UIButton("Test Button");
+
+            mLayout0 = new UIBoxLayout(eBoxLayoutOrientation.VERTICAL);
+            mLayout0.Add(mLabel0, true, true);
+            mLayout0.Add(mButton0, true, true);
+
             mMainForm.SetTitle("This is a test!!");
-            mMainForm.SetContent(mLabel0);
+            mMainForm.SetContent(mLayout0);
         }
 
         public void Shutdown() { }
 
         public void Update(float aTs)
         {
-            // Console.WriteLine("Update");
             mMainForm.Update();
         }
 
         public void UpdateUI(float aTs)
         {
-            // Console.WriteLine("Update");
-
         }
     }
 
