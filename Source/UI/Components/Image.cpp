1b916360a096cb85e5e87a8436be65392559adcf
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-25 20:04:21 -0400

    Added bindings for imagesd and imge buttons

--- Source/UI/Components/Image.cpp
+++ Source/UI/Components/Image.cpp
@@ -1,4 +1,5 @@
 #include "Image.h"
+#include "Mono/MonoRuntime.h"
 
 namespace SE::Core
 {
@@ -20,5 +21,22 @@ namespace SE::Core
 
         ImGui::Image( TextureID(), mSize, mTopLeft, mBottomRight, mTintColor, ImVec4{} );
     }
+
+    void *UIImage::UIImage_Create()
+    {
+        auto lNewImage = new UIImage();
+
+        return static_cast<void *>( lNewImage );
+    }
+
+    void *UIImage::UIImage_CreateWithPath( void *aText, math::vec2 *aSize )
+    {
+        auto lString   = MonoRuntime::NewString( static_cast<MonoString *>( aText ) );
+        auto lNewImage = new UIImage( lString, *aSize );
+
+        return static_cast<void *>( lNewImage );
+    }
+
+    void UIImage::UIImage_Destroy( void *aInstance ) { delete static_cast<UIImage *>( aInstance ); }
 
 } // namespace SE::Core
