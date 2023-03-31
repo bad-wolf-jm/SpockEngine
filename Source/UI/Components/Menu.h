f482b2cb5c3023e475b2ef3fa33f42ee7b4fc279
Jean-Martin Albert <jm.albert@gmail.com>
2023-03-30 22:36:54 -0400

    Menu and MenuItem implementation

--- Source/UI/Components/Menu.h
+++ Source/UI/Components/Menu.h
@@ -11,12 +11,20 @@ namespace SE::Core
         UIMenuItem() = default;
 
         UIMenuItem( std::string const &aText );
+        UIMenuItem( std::string const &aText, std::string const &aShortcut );
 
         void SetText( std::string const &aText );
+        void SetShortcut( std::string const &aShortcut );
         void SetTextColor( math::vec4 aColor );
+
+        void OnTrigger( std::function<void()> aOnTrigger );
+
+      protected:
+        std::function<void()> mOnTrigger;
 
       protected:
         std::string mText;
+        std::string mShortcut;
         ImVec4      mTextColor;
 
       protected:
@@ -26,35 +34,37 @@ namespace SE::Core
         ImVec2 RequiredSize();
         void   DrawContent( ImVec2 aPosition, ImVec2 aSize );
 
+      private:
+        void *mOnTriggerDelegate       = nullptr;
+        int   mOnTriggerDelegateHandle = -1;
+
       public:
         static void *UIMenuItem_Create();
         static void *UIMenuItem_CreateWithText( void *aText );
-        static void *UIMenu_CreateWithTextAndShortcut( void *aText, void *aShortcut );
+        static void *UIMenuItem_CreateWithTextAndShortcut( void *aText, void *aShortcut );
         static void  UIMenuItem_Destroy( void *aInstance );
         static void  UIMenuItem_SetText( void *aInstance, void *aText );
+        static void  UIMenuItem_SetShortcut( void *aInstance, void *aShortcut );
         static void  UIMenuItem_SetTextColor( void *aInstance, math::vec4 *aTextColor );
+        static void  UIMenuItem_OnTrigger( void *aInstance, void *aDelegate );
     };
 
     class UIMenu : public UIMenuItem
     {
       public:
         UIMenu() = default;
-
         UIMenu( std::string const &aText );
 
-        void SetText( std::string const &aText );
-        void SetTextColor( math::vec4 aColor );
-        void SetShortcut( std::string const &aText );
-
         UIMenuItem *AddAction( std::string const &aText, std::string const &aShortcut );
+        UIMenu     *AddMenu( std::string const &aText );
+
         UIMenuItem *AddSeparator();
-        UIMenuItem *AddMenu( std::string const &aText );
 
       protected:
         std::string mText;
         ImVec4      mTextColor;
 
-        std::vector<UIMenuItem *> mItems;
+        std::vector<UIMenuItem *> mActions;
 
       protected:
         void PushStyles();
@@ -66,9 +76,6 @@ namespace SE::Core
       public:
         static void *UIMenu_Create();
         static void *UIMenu_CreateWithText( void *aText );
-        static void *UIMenu_CreateWithTextAndShortcut( void *aText, void *aShortcut );
         static void  UIMenu_Destroy( void *aInstance );
-        static void  UIMenu_SetText( void *aInstance, void *aText );
-        static void  UIMenu_SetTextColor( void *aInstance, math::vec4 *aTextColor );
-    }
+    };
 } // namespace SE::Core
