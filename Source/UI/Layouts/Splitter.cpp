77e2b6ad1bf37d6b2c684daba07796e3ac0bd2c5
Jean-Martin Albert <jm.albert@gmail.com>
2023-04-04 13:55:36 -0400

    Compile splitter code

--- Source/UI/Layouts/Splitter.cpp
+++ Source/UI/Layouts/Splitter.cpp
@@ -1,5 +1,7 @@
 #include "Splitter.h"
 
+#include "imgui_internal.h"
+
 namespace SE::Core
 {
 
@@ -19,37 +21,20 @@ namespace SE::Core
         for( auto const &lItem : mChildren )
         {
             ImVec2 lRequiredSize{};
-            if( lItem.mFixedSize > 0.0f )
-            {
-                if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
 
-                if( mOrientation == eBoxLayoutOrientation::HORIZONTAL )
-                {
-                    lWidth += lItem.mFixedSize;
-                    lHeight = math::max( lHeight, lRequiredSize.y );
-                }
-                else
-                {
-                    lHeight += lItem.mFixedSize;
-                    lWidth = math::max( lWidth, lRequiredSize.x );
-                }
+            if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
+
+            if( mOrientation == eBoxLayoutOrientation::HORIZONTAL )
+            {
+                lWidth += lRequiredSize.x;
+                lHeight = math::max( lHeight, lRequiredSize.y );
             }
             else
             {
-
-                if( lItem.mItem ) lRequiredSize = lItem.mItem->RequiredSize();
-
-                if( mOrientation == eBoxLayoutOrientation::HORIZONTAL )
-                {
-                    lWidth += lRequiredSize.x;
-                    lHeight = math::max( lHeight, lRequiredSize.y );
-                }
-                else
-                {
-                    lHeight += lRequiredSize.y;
-                    lWidth = math::max( lWidth, lRequiredSize.x );
-                }
+                lHeight += lRequiredSize.y;
+                lWidth = math::max( lWidth, lRequiredSize.x );
             }
+            // }
         }
 
         return ImVec2{ lWidth, lHeight };
@@ -67,6 +52,30 @@ namespace SE::Core
         mChildren.push_back( SplitterItem{ aChild, false, aHAlignment, aVAlignment } );
     }
 
+    static bool Splitter( eBoxLayoutOrientation aOrientation, float aThickness, float *aSize1, float *aSize2, float aMinSize1,
+                          float aMinSize2 )
+    {
+        using namespace ImGui;
+        ImGuiContext &g      = *GImGui;
+        ImGuiWindow  *window = g.CurrentWindow;
+        ImGuiID       id     = window->GetID( "##Splitter" );
+        ImRect        bb;
+
+        if( aOrientation == eBoxLayoutOrientation::VERTICAL )
+        {
+            bb.Min = window->DC.CursorPos + ImVec2( *aSize1, 0.0f );
+            bb.Max = bb.Min + CalcItemSize( ImVec2( aThickness, -1.0f ), 0.0f, 0.0f );
+        }
+        else
+        {
+            bb.Min = window->DC.CursorPos + ImVec2( 0.0f, *aSize1 );
+            bb.Max = bb.Min + CalcItemSize( ImVec2( -1.0f, aThickness ), 0.0f, 0.0f );
+        }
+
+        ImGuiAxis lSplitDirection = ( aOrientation == eBoxLayoutOrientation::VERTICAL ) ? ImGuiAxis_X : ImGuiAxis_Y;
+        return SplitterBehavior( bb, id, lSplitDirection, aSize1, aSize2, aMinSize1, aMinSize2, 0.0f );
+    }
+
     void UISplitter::DrawContent( ImVec2 aPosition, ImVec2 aSize )
     {
         std::vector<float> lCumulativeSizes( mChildren.size() );
@@ -76,25 +85,23 @@ namespace SE::Core
         for( uint32_t i = mChildren.size() - 2; i >= 0; i-- ) lCumulativeSizes[i] = lCumulativeSizes[i + 1] + mItemSizes[i];
 
         ImVec2 lCurrentPosition = aPosition;
-        for (i=0; i < mChildren.size(); i++)
+        for( uint32_t i = 0; i < mChildren.size(); i++ )
         {
-            ImVec2 lItemSize{};
+            auto  &lItem = mChildren[i];
             ImVec2 lItemPosition{};
             float  lPositionStep = 0.0f;
 
-            lItemSize = lItem.mItem->RequiredSize();
-
+            ImVec2 lItemSize{};
             if( mOrientation == eBoxLayoutOrientation::VERTICAL )
-                lItemSize.x = aSize.x;
+                lItemSize = ImVec2{ aSize.x, mItemSizes[i] };
             else
-                lItemSize.y = aSize.y;
+                lItemSize = ImVec2{ mItemSizes[i], aSize.y };
 
             lItemSize     = lItem.mFill ? lItemSize : ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} );
             lItemPosition = lItem.mFill
                                 ? lCurrentPosition
                                 : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
-                                                                ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} ), lItemSize
-                                                                );
+                                                             ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} ), lItemSize );
             lPositionStep = ( mOrientation == eBoxLayoutOrientation::VERTICAL ) ? lItemSize.y : lItemSize.x;
 
             if( lItem.mItem ) lItem.mItem->Update( lItemPosition, lItemSize );
@@ -103,122 +110,39 @@ namespace SE::Core
                 lCurrentPosition.y += ( lPositionStep + mItemSpacing );
             else
                 lCurrentPosition.x += ( lPositionStep + mItemSpacing );
-
         }
-
-        // uint32_t lExpandCount = 0;
-        // float    lTaken       = mItemSpacing * ( mChildren.size() - 1 );
-        // for( auto const &lItem : mChildren )
-        // {
-        //     if( lItem.mFixedSize > 0.0f )
-        //     {
-        //         lTaken += lItem.mFixedSize;
-        //     }
-        //     else if( !lItem.mExpand )
-        //     {
-        //         auto lRequiredSize = lItem.mItem->RequiredSize();
-        //         lTaken += ( mOrientation == eBoxLayoutOrientation::HORIZONTAL ) ? lRequiredSize.x : lRequiredSize.y;
-        //     }
-        //     else
-        //     {
-        //         lExpandCount += 1;
-        //     }
-        // }
-
-        // ImVec2 lExpandedSize{};
-
-        // if( mOrientation == eBoxLayoutOrientation::HORIZONTAL )
-        //     lExpandedSize = ImVec2{ ( aSize.x - lTaken ) / lExpandCount, aSize.y };
-        // else
-        //     lExpandedSize = ImVec2{ aSize.x, ( aSize.y - lTaken ) / lExpandCount };
-
-        // ImVec2 lCurrentPosition = aPosition;
-        // for( auto const &lItem : mChildren )
-        // {
-        //     ImVec2 lItemSize{};
-        //     ImVec2 lItemPosition{};
-        //     float  lPositionStep = 0.0f;
-
-        //     if( lItem.mExpand && !( lItem.mFixedSize > 0.0f ) )
-        //     {
-        //         lItemSize     = lItem.mFill ? lExpandedSize : ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} );
-        //         lItemPosition = lItem.mFill ? lCurrentPosition
-        //                                     : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition, lItemSize,
-        //                                                                  lExpandedSize );
-        //         lPositionStep = ( mOrientation == eBoxLayoutOrientation::VERTICAL ) ? lExpandedSize.y : lExpandedSize.x;
-        //     }
-        //     else if( lItem.mFixedSize > 0.0f )
-        //     {
-        //         if( mOrientation == eBoxLayoutOrientation::VERTICAL )
-        //             lItemSize = ImVec2{ aSize.x, lItem.mFixedSize };
-        //         else
-        //             lItemSize = ImVec2{ lItem.mFixedSize, aSize.y };
-
-        //         lItemSize     = lItem.mFill ? lItemSize : ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} );
-        //         lItemPosition = lItem.mFill
-        //                             ? lCurrentPosition
-        //                             : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
-        //                                                          ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} ), lItemSize
-        //                                                          );
-        //         lPositionStep = lItem.mFixedSize;
-        //     }
-        //     else
-        //     {
-        //         lItemSize = lItem.mItem->RequiredSize();
-
-        //         if( mOrientation == eBoxLayoutOrientation::VERTICAL )
-        //             lItemSize.x = aSize.x;
-        //         else
-        //             lItemSize.y = aSize.y;
-
-        //         lItemSize     = lItem.mFill ? lItemSize : ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} );
-        //         lItemPosition = lItem.mFill
-        //                             ? lCurrentPosition
-        //                             : GetContentAlignedposition( lItem.mHalign, lItem.mValign, lCurrentPosition,
-        //                                                          ( lItem.mItem ? lItem.mItem->RequiredSize() : ImVec2{} ), lItemSize
-        //                                                          );
-        //         lPositionStep = ( mOrientation == eBoxLayoutOrientation::VERTICAL ) ? lItemSize.y : lItemSize.x;
-        //     }
-
-        //     if( lItem.mItem ) lItem.mItem->Update( lItemPosition, lItemSize );
-
-        //     if( mOrientation == eBoxLayoutOrientation::VERTICAL )
-        //         lCurrentPosition.y += ( lPositionStep + mItemSpacing );
-        //     else
-        //         lCurrentPosition.x += ( lPositionStep + mItemSpacing );
     }
-}
 
-void *UISplitter::UISplitter_CreateWithOrientation( eBoxLayoutOrientation aOrientation )
-{
-    auto lNewLayout = new UISplitter( aOrientation );
+    void *UISplitter::UISplitter_CreateWithOrientation( eBoxLayoutOrientation aOrientation )
+    {
+        auto lNewLayout = new UISplitter( aOrientation );
 
-    return static_cast<void *>( lNewLayout );
-}
+        return static_cast<void *>( lNewLayout );
+    }
 
-void UISplitter::UISplitter_Destroy( void *aInstance ) { delete static_cast<UISplitter *>( aInstance ); }
+    void UISplitter::UISplitter_Destroy( void *aInstance ) { delete static_cast<UISplitter *>( aInstance ); }
 
-void UISplitter::UISplitter_AddFill( void *aInstance, void *aChild )
-{
-    auto lInstance = static_cast<UISplitter *>( aInstance );
-    auto lChild    = static_cast<UIComponent *>( aChild );
+    void UISplitter::UISplitter_AddFill( void *aInstance, void *aChild )
+    {
+        auto lInstance = static_cast<UISplitter *>( aInstance );
+        auto lChild    = static_cast<UIComponent *>( aChild );
 
-    lInstance->Add( lChild );
-}
+        lInstance->Add( lChild );
+    }
 
-void UISplitter::UISplitter_AddAligned( void *aInstance, void *aChild, eHorizontalAlignment aHAlignment,
-                                        eVerticalAlignment aVAlignment )
-{
-    auto lInstance = static_cast<UISplitter *>( aInstance );
-    auto lChild    = static_cast<UIComponent *>( aChild );
+    void UISplitter::UISplitter_AddAligned( void *aInstance, void *aChild, eHorizontalAlignment aHAlignment,
+                                            eVerticalAlignment aVAlignment )
+    {
+        auto lInstance = static_cast<UISplitter *>( aInstance );
+        auto lChild    = static_cast<UIComponent *>( aChild );
 
-    lInstance->Add( lChild, aHAlignment, aVAlignment );
-}
+        lInstance->Add( lChild, aHAlignment, aVAlignment );
+    }
 
-void UISplitter::UISplitter_SetItemSpacing( void *aInstance, float aItemSpacing )
-{
-    auto lInstance = static_cast<UISplitter *>( aInstance );
+    void UISplitter::UISplitter_SetItemSpacing( void *aInstance, float aItemSpacing )
+    {
+        auto lInstance = static_cast<UISplitter *>( aInstance );
 
-    lInstance->SetItemSpacing( aItemSpacing );
-}
+        lInstance->SetItemSpacing( aItemSpacing );
+    }
 } // namespace SE::Core
