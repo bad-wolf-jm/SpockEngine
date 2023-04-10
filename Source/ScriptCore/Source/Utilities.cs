ecd4d00fb9ae6f0a3bce4137eeb33adea08f0f31
Jean-Martin Albert <jm.albert@gmail.com>
2023-04-10 13:22:19 -0400

    Derivec class discovery

---
+++ Source/ScriptCore/Source/Utilities.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpockEngine
+{
+    public static class Utilities
+    {
+        public static IEnumerable<Type> GetAllDerivedTypes<T>(bool aIncludeAllAssemblies = true)
+        {
+            var lType = typeof(T);
+
+            List<Type> lDerivedTypes = GetAllDerivedTypes<T>(Assembly.GetAssembly(lType)).ToList();
+
+            if (aIncludeAllAssemblies)
+            {
+                IEnumerable<Assembly> lDependentAssemblies = GetDependentAssemblies(Assembly.GetAssembly(lType));
+
+                lDerivedTypes.AddRange(lDependentAssemblies.SelectMany(a => GetAllDerivedTypes<T>(a)));
+            }
+
+            return lDerivedTypes;
+        }
+
+        private static IEnumerable<Assembly> GetDependentAssemblies(Assembly aAnalyzedAssembly)
+        {
+            return AppDomain.CurrentDomain.GetAssemblies().Where(aAssembly =>
+            {
+                return aAssembly.GetReferencedAssemblies()
+                                .Select(a => a.FullName)
+                                .Contains(aAnalyzedAssembly.FullName);
+            });
+        }
+
+        private static IEnumerable<Type> GetAllDerivedTypes<T>(Assembly aAssembly)
+        {
+            var lBaseType = typeof(T);
+            TypeInfo lBaseTypeInfo = lBaseType.GetTypeInfo();
+
+            return aAssembly.DefinedTypes.Where(aType =>
+            {
+                if (lBaseTypeInfo.IsClass)
+                    return aType.IsSubclassOf(lBaseType);
+
+                return lBaseTypeInfo.IsInterface && aType.ImplementedInterfaces.Contains(lBaseTypeInfo.AsType());
+
+            }).Select(aType => aType.AsType());
+        }
+    }
+}
