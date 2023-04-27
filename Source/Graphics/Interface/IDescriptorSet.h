71166e6fd61f68f55609e4c8c1b1d49b9aecd6e0
Jean-Martin Albert <jm.albert@gmail.com>
2023-04-27 09:46:54 -0400

    Code formatting

--- Source/Graphics/Interface/IDescriptorSet.h
+++ Source/Graphics/Interface/IDescriptorSet.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "Core/Memory.h"
+#include "IGraphicBuffer.h"
 #include "IGraphicContext.h"
 #include "ISampler2D.h"
 #include "ISamplerCubeMap.h"
-#include "IGraphicBuffer.h"
 
 #include <vector>
 #include <vulkan/vulkan.h>
@@ -17,9 +17,8 @@ namespace SE::Graphics
         IDescriptorSet( Ref<IGraphicContext> aGraphicContext, uint32_t aDescriptorCount = 0 );
         ~IDescriptorSet() = default;
 
-        void Write( Ref<ISampler2D> aBuffer, uint32_t aBinding );
-        void Write( Ref<ISamplerCubeMap> aBuffer, uint32_t aBinding );
-
+        void         Write( Ref<ISampler2D> aBuffer, uint32_t aBinding );
+        void         Write( Ref<ISamplerCubeMap> aBuffer, uint32_t aBinding );
         virtual void Write( Ref<IGraphicBuffer> aBuffer, bool aDynamicOffset, uint32_t aOffset, uint32_t aSize,
                             uint32_t aBinding )                                            = 0;
         virtual void Write( std::vector<Ref<ISampler2D>> aBuffer, uint32_t aBinding )      = 0;
