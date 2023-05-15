a2203ef9adb79c91d40b7b29cbbf17384ffea51a
Jean-Martin Albert <jm.albert@gmail.com>
2023-05-14 22:21:38 -0400

    Added better descriptor set abstraction

--- Source/Graphics/Interface/IDescriptorSet.h
+++ Source/Graphics/Interface/IDescriptorSet.h
@@ -9,12 +9,21 @@
 #include <vector>
 #include <vulkan/vulkan.h>
 
+#include "Graphics/Vulkan/VkPipeline.h"
+
 namespace SE::Graphics
 {
+    struct sDescriptorBindingInfo
+    {
+        uint32_t        mBindingIndex = 0;
+        eDescriptorType mType         = eDescriptorType::UNIFORM_BUFFER;
+        ShaderStageType mShaderStages = {};
+    };
+
     class IDescriptorSet
     {
       public:
-        IDescriptorSet( Ref<IGraphicContext> aGraphicContext, uint32_t aDescriptorCount = 0 );
+        IDescriptorSet( Ref<IGraphicContext> aGraphicContext, bool aIsUnbounded = false, uint32_t aDescriptorCount = 0 );
         ~IDescriptorSet() = default;
 
         void         Write( Ref<ISampler2D> aBuffer, uint32_t aBinding );
@@ -24,8 +33,16 @@ namespace SE::Graphics
         virtual void Write( std::vector<Ref<ISampler2D>> aBuffer, uint32_t aBinding )      = 0;
         virtual void Write( std::vector<Ref<ISamplerCubeMap>> aBuffer, uint32_t aBinding ) = 0;
 
+        void         AddBinding( uint32_t aBindingIndex, eDescriptorType aType, ShaderStageType aShaderStages );
+        virtual void Build() = 0;
+
       private:
         Ref<IGraphicContext> mGraphicContext{};
+
+        bool     mIsUnbounded     = false;
+        uint32_t mDescriptorCount = 1;
+
+        std::vector<sDescriptorBindingInfo> mDescriptors = {};
     };
 
 } // namespace SE::Graphics
