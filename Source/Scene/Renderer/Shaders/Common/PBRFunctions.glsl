5c182a7571815d50611adafc1b7fa29602c0d80f
Jean-Martin Albert <jm.albert@gmail.com>
2023-05-24 17:23:10 -0400

    Code formatting

--- Source/Scene/Renderer/Shaders/Common/PBRFunctions.glsl
+++ Source/Scene/Renderer/Shaders/Common/PBRFunctions.glsl
@@ -65,7 +65,10 @@ float GeometrySmith( vec3 N, vec3 V, vec3 L, float roughness )
 }
 
 // ----------------------------------------------------------------------------
-vec3 FresnelSchlick( float cosTheta, vec3 F0 ) { return F0 + ( vec3( 1.0 ) - F0 ) * pow( clamp( 1.0 - cosTheta, 0.0, 1.0 ), 5.0 ); }
+vec3 FresnelSchlick( float cosTheta, vec3 F0 ) 
+{ 
+    return F0 + ( vec3( 1.0 ) - F0 ) * pow( clamp( 1.0 - cosTheta, 0.0, 1.0 ), 5.0 ); 
+}
 
 vec3 CookTorrance( vec3 F0, vec3 N, vec3 L, vec3 V, vec3 H, float roughness )
 {
