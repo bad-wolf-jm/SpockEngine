96a575ce24aa3eec282a790feacd6ac6ce0c9db3
Jean-Martin Albert <jm.albert@gmail.com>
2023-05-25 14:23:13 -0400

    Hook into C# to create windows

---
+++ Programs/Editor/Source/MaterialEditor.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using SpockEngine;
+using Math = SpockEngine.Math;
+
+namespace SEEditor
+{
+    public class UIMaterialEditor : UIForm
+    {
+        public UIMaterialEditor() : base()
+        {
+            SetTitle("EDIT MATERIAL");
+        }
+
+        public void Update()
+        {
+            base.Update();
+        }
+    }
+}
