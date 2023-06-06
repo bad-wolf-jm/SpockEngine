9b43842da00ca96e57661eda7be929fea3e0891d
Jean-Martin Albert <jm.albert@gmail.com>
2023-06-05 22:41:36 -0400

    Added base class for vector editor

--- Source/UI/Components/VectorEdit.h
+++ Source/UI/Components/VectorEdit.h
@@ -4,33 +4,25 @@
 
 namespace SE::Core
 {
-    class UIVector3Input : public UIComponent
+    class UIVectorInputBase : public UIComponent
     {
       public:
-        UIVector3Input() = default;
+        UIVectorInputBase( int aDimension );
 
-        UIVector3Input( std::string const &aText );
-
-        void SetText( std::string const &aText );
-        void SetTextColor( math::vec4 aColor );
+        UIVectorInputBase( std::string const &aText );
 
         ImVec2 RequiredSize();
-        
+
       protected:
-        std::string mText;
-        ImVec4      mTextColor;
+        int         mDimension{};
+        math::vec4  mValues{};
+        math::vec4  mResetValues{};
+        std::string mFormat = ".2f";
 
       protected:
         void PushStyles();
         void PopStyles();
 
-        void   DrawContent( ImVec2 aPosition, ImVec2 aSize );
-
-      public:
-        static void *UILabel_Create();
-        static void *UILabel_CreateWithText( void *aText );
-        static void  UILabel_Destroy( void *aInstance );
-        static void  UILabel_SetText( void *aInstance, void *aText );
-        static void  UILabel_SetTextColor( void *aInstance, math::vec4 aTextColor );
+        void DrawContent( ImVec2 aPosition, ImVec2 aSize );
     };
 } // namespace SE::Core
