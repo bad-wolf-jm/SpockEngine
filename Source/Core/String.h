8c0eed043b23710da0810dc864aed5cd2027efa7
Jean-Martin Albert <jm.albert@gmail.com>
2023-06-15 10:04:51 -0400

    Added types for strings

---
+++ Source/Core/String.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+namespace SE::Core
+{
+    using char_t   = char;
+    using string_t = std::string;
+    using path_t   = std::filesystem::path;
+} // namespace SE::Core
