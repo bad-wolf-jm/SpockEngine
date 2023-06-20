734434bbc265927a4b455bf0d928fdf028648412
Jean-Martin Albert <jm.albert@gmail.com>
2023-06-19 23:58:15 -0400

    Create application folders on first launch

--- Programs/Bootstrap/Source/Main.cs
+++ Programs/Bootstrap/Source/Main.cs
@@ -1,4 +1,11 @@
 using System;
+using System.IO;
+using System.Linq;
+// using System.Drawing;
+using System.Drawing.Text;
+// using System.Windows.Media;
+using System.Collections.Generic;
+
 using CommandLine;
 using SpockEngine;
 
@@ -6,8 +13,52 @@ class SEBootstrap
 {
     public class Options
     {
-        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-        public bool Verbose { get; set; }
+        [Option('a', "application", Required = true, HelpText = "Run application")]
+        public string Application { get; set; }
+
+        [Option('x', Required = false, HelpText = "Window x position")]
+        public int X { get; set; }
+
+        [Option('y', Required = false, HelpText = "Window y position")]
+        public int Y { get; set; }
+
+        [Option('w', "width", Required = false, HelpText = "Window width")]
+        public int Width { get; set; }
+
+        [Option('h', "height", Required = false, HelpText = "Window height")]
+        public int Height { get; set; }
+    }
+
+    private static List<string> GetFilesForFont(string fontName)
+    {
+        var fontNameToFiles = new Dictionary<string, List<string>>();
+
+        foreach (var fontFile in Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Fonts)))
+        {
+            var fc = new PrivateFontCollection();
+
+            if (File.Exists(fontFile))
+                fc.AddFontFile(fontFile);
+
+            if ((!fc.Families.Any()))
+                continue;
+
+            var name = fc.Families[0].Name;
+
+            // If you care about bold, italic, etc, you can filter here.
+            if (!fontNameToFiles.TryGetValue(name, out var files))
+            {
+                files = new List<string>();
+                fontNameToFiles[name] = files;
+            }
+
+            files.Add(fontFile);
+        }
+
+        if (!fontNameToFiles.TryGetValue(fontName, out var result))
+            return null;
+
+        return result;
     }
 
     static void Main(string[] args)
@@ -16,11 +67,14 @@ class SEBootstrap
 
         var lUserHome = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         var lUserConfig = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        var lFontsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Fonts);
+
+
+        var lUIFont = GetFilesForFont("Segoe UI");
+        foreach( var x in lUIFont)
+            Console.WriteLine(x);
 
         Console.WriteLine(lUserHome);
         Console.WriteLine(lUserConfig);
-        Console.WriteLine(lFontsFolder);
         Console.WriteLine(lExePath);
 
         Parser
@@ -28,16 +82,18 @@ class SEBootstrap
             .ParseArguments<Options>(args)
             .WithParsed<Options>(o =>
             {
-                if (o.Verbose)
-                {
-                    Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-                    Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-                }
-                else
-                {
-                    Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-                    Console.WriteLine("Quick Start Example!");
-                }
+                var lAppConfigRoot = Path.Combine(lUserConfig, o.Application);
+
+                var lAppConfig = Path.Combine(lAppConfigRoot, "Config");
+                if (!Directory.Exists(lAppConfig))
+                    Directory.CreateDirectory(lAppConfig);
+
+                var lAppLogs = Path.Combine(lAppConfigRoot, "Logs");
+                if (!Directory.Exists(lAppLogs))
+                    Directory.CreateDirectory(lAppLogs);
+
+                var lAppUIConfig = Path.Combine(lAppConfig, "imgui.ini");
+                var lAppIniConfig = Path.Combine(lAppConfig, "Application.yaml");
             });
 
     }
