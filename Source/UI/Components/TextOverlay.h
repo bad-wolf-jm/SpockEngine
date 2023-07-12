20f3b18b8ba3051981395dc6dfd4833b8de2ade1
Jean-Martin Albert <jm.albert@gmail.com>
2023-07-12 16:37:03 -0400

    Made the test overlay class behave more like a console output

--- Source/UI/Components/TextOverlay.h
+++ Source/UI/Components/TextOverlay.h
@@ -13,18 +13,38 @@ namespace SE::Core
         bool     mIsPartial   = true;
     };
 
+    enum class eTextEncoding : int32_t
+    {
+        UTF16 = 0,
+        UTF8  = 1,
+        ASCII = 2
+    };
+
+    struct sChar
+    {
+        char mCharacter;
+        char mWidth;
+
+        sChar( char aCharacter, char aWidth )
+            : mCharacter{ aCharacter }
+            , mWidth{ aWidth }
+        {
+        }
+    };
+
     class UITextOverlay : public UIComponent
     {
       public:
         UITextOverlay() = default;
 
         void AddText( string_t const &aText );
+        void AddText( char *aBytes, int32_t aOffset, int32_t aCount );
         void Clear();
 
       protected:
-        uint32_t             mLineCount = 0;
-        std::list<sTextLine> mLines;
-        string_t             mLeftOver;
+        // uint32_t             mLineCount = 0;
+        // std::list<sTextLine> mLines;
+        // string_t             mLeftOver;
 
         uint32_t mMaxLineCount = 100000;
 
@@ -32,6 +52,9 @@ namespace SE::Core
         uint32_t mCharHeight    = 0;
         uint32_t mConsoleWidth  = 0;
         uint32_t mConsoleHeight = 0;
+
+        std::vector<sChar> mCharacters;
+        eTextEncoding      mEncoding = eTextEncoding::UTF16;
 
       protected:
         void PushStyles();
