dc3c4536b14f36de399c01839a93a405e1b1a98c
Jean-Martin Albert <jm.albert@gmail.com>
2023-07-16 11:12:11 -0400

    Copied renderer files to new location

---
+++ Source/Renderer2/EffectProcessor.cpp
@@ -0,0 +1,41 @@
+#include "EffectProcessor.h"
+#include "Core/Math/Types.h"
+#include "Core/Resource.h"
+
+namespace SE::Core
+{
+    using namespace SE::Graphics;
+
+    EffectProcessor::EffectProcessor( Ref<IGraphicContext> aGraphicContext, Ref<IRenderContext> aRenderContext,
+                                      EffectProcessorCreateInfo aCreateInfo )
+        : mGraphicContext{ aGraphicContext }
+        , Spec{ aCreateInfo }
+    {
+        mPipeline = CreateGraphicsPipeline( mGraphicContext, aRenderContext, ePrimitiveTopology::TRIANGLES );
+
+        mPipeline->SetCulling( eFaceCulling::NONE );
+        mPipeline->SetLineWidth( 1.0f );
+        mPipeline->SetShader( eShaderStageTypeFlags::VERTEX, Spec.mVertexShader, "main" );
+        mPipeline->SetShader( eShaderStageTypeFlags::FRAGMENT, Spec.mFragmentShader, "main" );
+
+        PipelineLayout = CreateDescriptorSetLayout( mGraphicContext );
+        PipelineLayout->AddBinding( 0, eDescriptorType::COMBINED_IMAGE_SAMPLER, { eShaderStageTypeFlags::FRAGMENT } );
+        PipelineLayout->Build();
+        mPipeline->AddDescriptorSet( PipelineLayout );
+
+        mPipeline->Build();
+
+        mTextures = PipelineLayout->Allocate();
+    }
+
+    void EffectProcessor::Render( Ref<ISampler2D> aImageSampler, Ref<IRenderContext> aRenderContext )
+    {
+        mTextures->Write( aImageSampler, 0 );
+
+        aRenderContext->Bind( mPipeline );
+        aRenderContext->Bind( mTextures, 0, -1 );
+        aRenderContext->ResetBuffers();
+        aRenderContext->Draw( 6, 0, 0, 1, 0 );
+    }
+
+} // namespace SE::Core
