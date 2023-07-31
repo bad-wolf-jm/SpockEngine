73828c2b0acad599fddac35c54def0a13af840da
Jean-Martin Albert <jm.albert@gmail.com>
2023-07-31 10:46:24 -0400

    Added shader code for coordinate grid

--- Shaders/Source/Renderer/CoordinateGrid.hpp
+++ Shaders/Source/Renderer/CoordinateGrid.hpp
@@ -18,12 +18,12 @@ LAYOUT_UNIFORM( 0 ) ViewUniforms
 #    define INOUT
 #endif
 
-LAYOUT_LOCATION( 0 ) INOUT float outNear; // 0.01
-LAYOUT_LOCATION( 1 ) INOUT float outFar;  // 100
-LAYOUT_LOCATION( 2 ) INOUT float3 outNearPoint;
-LAYOUT_LOCATION( 3 ) INOUT float3 outFarPoint;
-LAYOUT_LOCATION( 4 ) INOUT float4x4 outFragView;
-LAYOUT_LOCATION( 8 ) INOUT float4x4 outFragProj;
+LAYOUT_LOCATION( 0 ) INOUT float gNear; // 0.01
+LAYOUT_LOCATION( 1 ) INOUT float gFar;  // 100
+LAYOUT_LOCATION( 2 ) INOUT float3 gNearPoint;
+LAYOUT_LOCATION( 3 ) INOUT float3 gFarPoint;
+LAYOUT_LOCATION( 4 ) INOUT float4x4 gFragView;
+LAYOUT_LOCATION( 8 ) INOUT float4x4 gFragProj;
 
 // Grid position are in clipped space
 #if defined( COORDINATE_GRID_VERTEX_SHADER )
@@ -43,13 +43,13 @@ float3 UnprojectPoint( float x, float y, float z, float4x4 aView, float4x4 aProj
 #if defined( COORDINATE_GRID_FRAGMENT_SHADER )
 float4 grid( float3 fragPos3D, float scale, bool drawAxis )
 {
-    vec2  coord      = fragPos3D.xz * scale;
-    vec2  derivative = fwidth( coord );
-    vec2  grid       = abs( fract( coord - 0.5 ) - 0.5 ) / derivative;
-    float line       = min( grid.x, grid.y );
-    float minimumz   = min( derivative.y, 1 );
-    float minimumx   = min( derivative.x, 1 );
-    float4  color      = float4( 1.0, 1.0, 1.0, 1.0 - min( line, 1.0 ) );
+    vec2   coord      = fragPos3D.xz * scale;
+    vec2   derivative = fwidth( coord );
+    vec2   grid       = abs( fract( coord - 0.5 ) - 0.5 ) / derivative;
+    float  line       = min( grid.x, grid.y );
+    float  minimumz   = min( derivative.y, 1 );
+    float  minimumx   = min( derivative.x, 1 );
+    float4 color      = float4( 1.0, 1.0, 1.0, 1.0 - min( line, 1.0 ) );
 
     // z axis
     if( fragPos3D.x > -minimumx && fragPos3D.x < minimumx )
@@ -64,18 +64,18 @@ float4 grid( float3 fragPos3D, float scale, bool drawAxis )
 
 float computeDepth( float3 pos )
 {
-    float4 clip_space_pos = fragProj * fragView * float4( pos.xyz, 1.0 );
+    float4 clip_space_pos = gFragProj * gFragView * float4( pos.xyz, 1.0 );
     return ( clip_space_pos.z / clip_space_pos.w );
 }
 
 float computeLinearDepth( float3 pos )
 {
-    float4  clip_space_pos   = fragProj * fragView * float4( pos.xyz, 1.0 );
-    float clip_space_depth = ( clip_space_pos.z / clip_space_pos.w ) * 2.0 - 1.0;  // put back between -1 and 1
-    float linearDepth =
-        ( 2.0 * near * far ) / ( far + near - clip_space_depth * ( far - near ) ); // get linear value between 0.01 and 100
+    float4 clip_space_pos   = gFragProj * gFragView * float4( pos.xyz, 1.0 );
+    float  clip_space_depth = ( clip_space_pos.z / clip_space_pos.w ) * 2.0 - 1.0;       // put back between -1 and 1
+    float  linearDepth =
+        ( 2.0 * gNear * gFar ) / ( gFar + gNear - clip_space_depth * ( gFar - gNear ) ); // get linear value between 0.01 and 100
 
-    return linearDepth / far;
+    return linearDepth / gFar;
 }
 
 #endif
@@ -83,19 +83,19 @@ float computeLinearDepth( float3 pos )
 void main()
 {
 #if defined( COORDINATE_GRID_VERTEX_SHADER )
-    float3 p     = gridPlane[gl_VertexIndex].xyz;
-    outNear      = 0.01;
-    outFar       = 25.0f;
-    outFragView  = gView.mViewMatrix;
-    outFragProj  = gView.mProjectionMatrix;
-    outNearPoint = UnprojectPoint( p.x, p.y, 0.0, gView.mViewMatrix, gView.mProjectionMatrix ).xyz; // unprojecting on the inNear plane
-    outFarPoint  = UnprojectPoint( p.x, p.y, 1.0, gView.mViewMatrix, gView.mProjectionMatrix ).xyz; // unprojecting on the inFar plane
-    gl_Position  = float4( p, 1.0 ); // using directly the clipped coordinates
+    float3 p    = gridPlane[gl_VertexIndex].xyz;
+    gNear       = 0.01;
+    gFar        = 25.0f;
+    gFragView   = gView.mViewMatrix;
+    gFragProj   = gView.mProjectionMatrix;
+    gNearPoint  = UnprojectPoint( p.x, p.y, 0.0, gView.mViewMatrix, gView.mProjectionMatrix ).xyz; // unprojecting on the inNear plane
+    gFarPoint   = UnprojectPoint( p.x, p.y, 1.0, gView.mViewMatrix, gView.mProjectionMatrix ).xyz; // unprojecting on the inFar plane
+    gl_Position = float4( p, 1.0 ); // using directly the clipped coordinates
 #endif
 
 #if defined( COORDINATE_GRID_FRAGMENT_SHADER )
-    float t           = -nearPoint.y / ( farPoint.y - nearPoint.y );
-    float3  fragPos3D   = nearPoint + t * ( farPoint - nearPoint );
+    float  t          = -gNearPoint.y / ( gFarPoint.y - gNearPoint.y );
+    float3 fragPos3D  = gNearPoint + t * ( gFarPoint - gNearPoint );
     gl_FragDepth      = computeDepth( fragPos3D );
     float linearDepth = computeLinearDepth( fragPos3D );
     float fading      = max( 0, ( .50 - linearDepth ) );
