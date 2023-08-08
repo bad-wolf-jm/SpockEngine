bb7cad0e1cf9eb700c43648f020e37eb0fdd4d82
Jean-Martin Albert <jm.albert@gmail.com>
2023-08-08 12:32:30 -0400

    Use a global converter for string

--- Source/Core/String.cpp
+++ Source/Core/String.cpp
@@ -1,28 +1,29 @@
 #include "Engine/Engine.h"
 
 #include "File.h"
+#include "Profiling/BlockTimer.h"
 #include "String.h"
 
 #include <codecvt>
 #include <locale>
 
+static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> gConverter;
+
 namespace SE::Core
 {
     string_t ConvertStringForCoreclr( wchar_t *aCharacters )
     {
-        std::wstring u16str( aCharacters );
+        SE_PROFILE_FUNCTION();
 
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> convert;
-        string_t                                                        utf8 = convert.to_bytes( u16str );
+        std::wstring u16str( aCharacters );
+        string_t     utf8 = gConverter.to_bytes( u16str );
 
         return utf8;
     }
 
     std::wstring ConvertStringForCoreclr( const string_t &utf8 )
     {
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> convert;
-
-        std::wstring utf16 = convert.from_bytes( utf8 );
+        std::wstring utf16 = gConverter.from_bytes( utf8 );
 
         return utf16;
     }
