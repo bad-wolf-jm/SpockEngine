5404709329c4452f5044b5b8dbb43fd8437f1202
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-21 19:45:01 -0400

    Code formatting

--- Source/Scripting/Utilities/common.h
+++ Source/Scripting/Utilities/common.h
@@ -3,5 +3,5 @@ namespace numlua::core::details
     int  do_isabsolute( const char *path );
     void do_translate( char *value, const char sep );
     void do_normalize( lua_State *L, char *buffer, const char *path );
-    int do_getcwd( char *buffer, size_t size );
+    int  do_getcwd( char *buffer, size_t size );
 } // namespace numlua::core::details
