81c94bf4a37df66c7005fcfc3cd37c892fa71342
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-24 17:06:15 -0400

    Refactored unit tests

--- Source/Core/LinearAlgebra/vector.h
+++ Source/Core/LinearAlgebra/vector.h
@@ -8,7 +8,7 @@ namespace numlua::linalg
 {
     using float2 = vect<2, float>;
     using float3 = vect<3, float>;
-    using float4 = vect<L, float>;
+    using float4 = vect<4, float>;
 
     template <size_t L, typename _Ty>
     SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<L, _Ty> operator+( vect<L, _Ty> const &v )
--- Tests/Test_LinearAlgebra.cpp
+++ Tests/Test_LinearAlgebra.cpp
@@ -76,7 +76,7 @@ TEST_CASE( "4D Vectors", "[LINEAR_ALGEBRA]" )
 TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
 {
     constexpr float scalarConstant = 4.5f;
-    
+
     {
         auto const v1 = linalg::float2( 1.0f, 2.0f );
         auto const v2 = linalg::float2( 2.0f, 3.0f );
@@ -85,10 +85,12 @@ TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
         auto       v5 = 4.5f + v1;
         auto       v6 = +v3;
 
-        REQUIRE( ( v3.x == 3.0f && v3.y == 5.0f ) );
-        REQUIRE( ( v4.x == 5.5f && v4.y == 6.5f ) );
-        REQUIRE( ( v5.x == 5.5f && v5.y == 6.5f ) );
-        REQUIRE( ( v6.x == 3.0f && v6.y == 5.0f ) );
+        // clang-format off
+        REQUIRE( ( (v3.x == v1.x + v2.x)           && (v3.y == v1.y + v2.y)           ) );
+        REQUIRE( ( (v4.x == v1.x + scalarConstant) && (v4.y == v1.y + scalarConstant) ) );
+        REQUIRE( ( (v5.x == v1.x + scalarConstant) && (v5.y == v1.y + scalarConstant) ) );
+        REQUIRE( ( v6.x == v3.x                    && v6.y == v3.y ) );
+        // clang-format on
     }
 
     {
@@ -99,23 +101,27 @@ TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
         auto       v5 = 4.5f + v1;
         auto       v6 = +v3;
 
-        REQUIRE( ( v3.x == 3.0f && v3.y == 5.0f & v3.z == 7.0f) );
-        REQUIRE( ( v4.x == 5.5f && v4.y == 6.5f & v4.z == 7.5f) );
-        REQUIRE( ( v5.x == 5.5f && v5.y == 6.5f & v5.z == 7.5f) );
-        REQUIRE( ( v6.x == 3.0f && v6.y == 5.0f & v6.z == 7.0f) );
+        // clang-format off
+        REQUIRE( ( (v3.x == v1.x + v2.x)           && (v3.y == v1.y + v2.y)           && (v3.z == v1.z + v2.z)           ) );
+        REQUIRE( ( (v4.x == v1.x + scalarConstant) && (v4.y == v1.y + scalarConstant) && (v4.z == v1.z + scalarConstant) ) );
+        REQUIRE( ( (v5.x == v1.x + scalarConstant) && (v5.y == v1.y + scalarConstant) && (v5.z == v1.z + scalarConstant) ) );
+        REQUIRE( ( v6.x == v3.x                    && v6.y == v3.y                    && v6.z == v3.z ) );
+        // clang-format on
     }
 
     {
         auto const v1 = linalg::float4( 1.0f, 2.0f, 3.0f, 4.0f );
         auto const v2 = linalg::float4( 2.0f, 3.0f, 4.0f, 5.0f );
         auto       v3 = v1 + v2;
-        auto       v4 = v1 + 4.5f;
-        auto       v5 = 4.5f + v1;
+        auto       v4 = v1 + scalarConstant;
+        auto       v5 = scalarConstant + v1;
         auto       v6 = +v3;
 
-        REQUIRE( ( v3.x == 3.0f && v3.y == 5.0f & v3.z == 7.0f & v3.w == 9.0f ) );
-        REQUIRE( ( v4.x == 5.5f && v4.y == 6.5f & v4.z == 7.5f & v4.w == 8.5f ) );
-        REQUIRE( ( v5.x == 5.5f && v5.y == 6.5f & v5.z == 7.5f & v5.w == 8.5f ) );
-        REQUIRE( ( v6.x == 3.0f && v6.y == 5.0f & v6.z == 7.0f & v6.w == 9.0f ) );
+        // clang-format off
+        REQUIRE( ( (v3.x == v1.x + v2.x)           && (v3.y == v1.y + v2.y)           && (v3.z == v1.z + v2.z)           && (v3.w == v1.w + v2.w) ) );
+        REQUIRE( ( (v4.x == v1.x + scalarConstant) && (v4.y == v1.y + scalarConstant) && (v4.z == v1.z + scalarConstant) && (v4.w == v1.w + scalarConstant) ) );
+        REQUIRE( ( (v5.x == v1.x + scalarConstant) && (v5.y == v1.y + scalarConstant) && (v5.z == v1.z + scalarConstant) && (v5.w == v1.w + scalarConstant) ) );
+        REQUIRE( ( v6.x == v3.x                    && v6.y == v3.y                    && v6.z == v3.z                    && v6.w == v3.w ) );
+        // clang-format on
     }
 }
