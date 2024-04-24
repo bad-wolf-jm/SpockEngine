5efe44ddbcfaedae0484c62a42e428ad922f9b16
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-24 14:49:39 -0400

    Added constructors and accessors

--- Source/Core/LinearAlgebra/vector2.h
+++ Source/Core/LinearAlgebra/vector2.h
@@ -17,6 +17,49 @@ namespace numlua::linalg
             struct { _Ty s, t; };
             // clang-format on
         };
+        // clang-format off
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<2, _Ty>( _Ty _x, _Ty _y )
+            : x{ _x } , y{ _y } { }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<2, _Ty>( _Ty _x )
+            : x{ _x } , y{ _x } { }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<2, _Ty>( vect<2, _Ty> const &v )
+            : x{ v.x } , y{ v.y } { }
+        // clang-format on
+
+        template <typename X, typename Y, typename Z, typename W>
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<2, _Ty>( X _x, Y _y )
+            : x( static_cast<_Ty>( _x ) )
+            , y( static_cast<_Ty>( _y ) )
+        {
+        }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr _Ty &operator[]( size_t i )
+        {
+            // GLM_ASSERT_LENGTH( i, this->length() );
+            switch( i )
+            {
+                // clang-format off
+            default:
+            case 0: return x;
+            case 1: return y;
+                // clang-format on
+            }
+        }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr _Ty const &operator[]( size_t i ) const
+        {
+            // GLM_ASSERT_LENGTH( i, this->length() );
+            switch( i )
+            {
+                // clang-format off
+            default:
+            case 0: return x;
+            case 1: return y;
+                // clang-format on
+            }
+        }
     };
 
 } // namespace numlua::linalg
--- Source/Core/LinearAlgebra/vector3.h
+++ Source/Core/LinearAlgebra/vector3.h
@@ -16,5 +16,68 @@ namespace numlua::linalg
             struct { _Ty s, t, p; };
             // clang-format on
         };
+
+        // clang-format off
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty>( _Ty _x, _Ty _y, _Ty _z )
+            : x{ _x } , y{ _y } , z{ _z } { }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty>( _Ty _x )
+            : x{ _x } , y{ _x } , z{ _x } { }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty>(vect<2, _Ty> const& v, _Ty _z )
+            : x{ v.x } , y{ v.y } , z{ _z } { }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty>(_Ty _x, vect<2,_Ty> const& v )
+            : x{ _x } , y{ v.x } , z{ v.y } { }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty>( vect<3, _Ty> const &v )
+            : x{ v.x } , y{ v.y } , z{ v.z } { }
+        // clang-format on
+
+        template <typename X, typename Y, typename Z, typename W>
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty>( X _x, Y _y, Z _z )
+            : x( static_cast<_Ty>( _x ) )
+            , y( static_cast<_Ty>( _y ) )
+            , z( static_cast<_Ty>( _z ) )
+        {
+        }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr _Ty &operator[]( size_t i )
+        {
+            // GLM_ASSERT_LENGTH( i, this->length() );
+            switch( i )
+            {
+                // clang-format off
+            default:
+            case 0: return x;
+            case 1: return y;
+            case 2: return z;
+                // clang-format on
+            }
+        }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr _Ty const &operator[]( size_t i ) const
+        {
+            // GLM_ASSERT_LENGTH( i, this->length() );
+            switch( i )
+            {
+                // clang-format off
+            default:
+            case 0: return x;
+            case 1: return y;
+            case 2: return z;
+                // clang-format on
+            }
+        }
+
+        SE_CUDA_HOST_DEVICE_FUNCTION_DEF constexpr vect<3, _Ty> &operator=( vect<3, _Ty> const &v )
+        {
+            this->x = static_cast<_Ty>( v.x );
+            this->y = static_cast<_Ty>( v.y );
+            this->z = static_cast<_Ty>( v.z );
+
+            return *this;
+        }
+
     };
 } // namespace numlua::linalg
