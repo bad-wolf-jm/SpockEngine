54e722b418bb0d7771becdfc2618f77183defd12
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-24 14:49:17 -0400

    Removed unused declarations

--- Source/Core/LinearAlgebra/vector.h
+++ Source/Core/LinearAlgebra/vector.h
@@ -6,12 +6,7 @@
 
 namespace numlua::linalg
 {
-    using int2   = vect<2, int32_t>;
     using float2 = vect<2, float>;
-
-    using int3   = vect<3, int32_t>;
     using float3 = vect<3, float>;
-
-    using int4    = vect<4, int32_t>;
     using float4 = vect<4, float>;
 } // namespace numlua::linalg
