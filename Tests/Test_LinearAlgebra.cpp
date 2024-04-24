31ea5105910fc681b2aa0edaf2ac8cd2261288fb
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-24 17:18:31 -0400

    Added unit tests for vector multiplication

--- Tests/Test_LinearAlgebra.cpp
+++ Tests/Test_LinearAlgebra.cpp
@@ -73,7 +73,7 @@ TEST_CASE( "4D Vectors", "[LINEAR_ALGEBRA]" )
     REQUIRE( ( v7.x == 1.0f && v7.y == 1.0f & v7.z == 2.0f & v7.w == 3.0f ) );
 }
 
-TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
+TEST_CASE( "Vector addition", "[LINEAR_ALGEBRA]" )
 {
     constexpr float scalarConstant = 4.5f;
 
@@ -81,8 +81,8 @@ TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
         auto const v1 = linalg::float2( 1.0f, 2.0f );
         auto const v2 = linalg::float2( 2.0f, 3.0f );
         auto       v3 = v1 + v2;
-        auto       v4 = v1 + 4.5f;
-        auto       v5 = 4.5f + v1;
+        auto       v4 = v1 + scalarConstant;
+        auto       v5 = scalarConstant + v1;
         auto       v6 = +v3;
 
         // clang-format off
@@ -97,8 +97,8 @@ TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
         auto const v1 = linalg::float3( 1.0f, 2.0f, 3.0f );
         auto const v2 = linalg::float3( 2.0f, 3.0f, 4.0f );
         auto       v3 = v1 + v2;
-        auto       v4 = v1 + 4.5f;
-        auto       v5 = 4.5f + v1;
+        auto       v4 = v1 + scalarConstant;
+        auto       v5 = scalarConstant + v1;
         auto       v6 = +v3;
 
         // clang-format off
@@ -122,6 +122,53 @@ TEST_CASE( "Algrbraic operations on vectors", "[LINEAR_ALGEBRA]" )
         REQUIRE( ( (v4.x == v1.x + scalarConstant) && (v4.y == v1.y + scalarConstant) && (v4.z == v1.z + scalarConstant) && (v4.w == v1.w + scalarConstant) ) );
         REQUIRE( ( (v5.x == v1.x + scalarConstant) && (v5.y == v1.y + scalarConstant) && (v5.z == v1.z + scalarConstant) && (v5.w == v1.w + scalarConstant) ) );
         REQUIRE( ( v6.x == v3.x                    && v6.y == v3.y                    && v6.z == v3.z                    && v6.w == v3.w ) );
+        // clang-format on
+    }
+}
+
+TEST_CASE( "Vector multiplication", "[LINEAR_ALGEBRA]" )
+{
+    constexpr float scalarConstant = 4.5f;
+
+    {
+        auto const v1 = linalg::float2( 1.0f, 2.0f );
+        auto const v2 = linalg::float2( 2.0f, 3.0f );
+        auto       v3 = v1 * v2;
+        auto       v4 = v1 * scalarConstant;
+        auto       v5 = scalarConstant * v1;
+
+        // clang-format off
+        REQUIRE( ( (v3.x == v1.x * v2.x)           && (v3.y == v1.y * v2.y)           ) );
+        REQUIRE( ( (v4.x == v1.x * scalarConstant) && (v4.y == v1.y * scalarConstant) ) );
+        REQUIRE( ( (v5.x == v1.x * scalarConstant) && (v5.y == v1.y * scalarConstant) ) );
+        // clang-format on
+    }
+
+    {
+        auto const v1 = linalg::float3( 1.0f, 2.0f, 3.0f );
+        auto const v2 = linalg::float3( 2.0f, 3.0f, 4.0f );
+        auto       v3 = v1 * v2;
+        auto       v4 = v1 * scalarConstant;
+        auto       v5 = scalarConstant * v1;
+
+        // clang-format off
+        REQUIRE( ( (v3.x == v1.x * v2.x)           && (v3.y == v1.y * v2.y)           && (v3.z == v1.z * v2.z)           ) );
+        REQUIRE( ( (v4.x == v1.x * scalarConstant) && (v4.y == v1.y * scalarConstant) && (v4.z == v1.z * scalarConstant) ) );
+        REQUIRE( ( (v5.x == v1.x * scalarConstant) && (v5.y == v1.y * scalarConstant) && (v5.z == v1.z * scalarConstant) ) );
+        // clang-format on
+    }
+
+    {
+        auto const v1 = linalg::float4( 1.0f, 2.0f, 3.0f, 4.0f );
+        auto const v2 = linalg::float4( 2.0f, 3.0f, 4.0f, 5.0f );
+        auto       v3 = v1 * v2;
+        auto       v4 = v1 * scalarConstant;
+        auto       v5 = scalarConstant * v1;
+
+        // clang-format off
+        REQUIRE( ( (v3.x == v1.x * v2.x)           && (v3.y == v1.y * v2.y)           && (v3.z == v1.z * v2.z)           && (v3.w == v1.w * v2.w) ) );
+        REQUIRE( ( (v4.x == v1.x * scalarConstant) && (v4.y == v1.y * scalarConstant) && (v4.z == v1.z * scalarConstant) && (v4.w == v1.w * scalarConstant) ) );
+        REQUIRE( ( (v5.x == v1.x * scalarConstant) && (v5.y == v1.y * scalarConstant) && (v5.z == v1.z * scalarConstant) && (v5.w == v1.w * scalarConstant) ) );
         // clang-format on
     }
 }
