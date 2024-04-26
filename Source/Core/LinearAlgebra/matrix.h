ed5e5955e836679e1a68c068906e0b12a0b99314
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-26 12:20:13 -0400

    Changed template variable names to shorter versions

--- Source/Core/LinearAlgebra/matrix.h
+++ Source/Core/LinearAlgebra/matrix.h
@@ -7,28 +7,28 @@ namespace numlua::linalg
 {
     using float2x2 = mat<2, 2, float>;
 
-    template <size_t _Rows, size_t _Columns, typename T>
-    LINALG_FUNCTION matrix<_Rows, _Columns, T> operator+( matrix<2, 2, T> const &m )
+    template <size_t _R, size_t _C, typename T>
+    LINALG_FUNCTION matrix<_R, _C, T> operator+( matrix<2, 2, T> const &m )
     {
         return m;
     }
 
-    template <size_t _Rows, size_t _Columns, typename T>
-    LINALG_FUNCTION typename matrix<_Rows, _Columns, T>::col_type operator/( matrix<_Rows, _Columns, T> const                    &m,
-                                                                             typename matrix<_Rows, _Columns, T>::row_type const &v )
+    template <size_t _R, size_t _C, typename T>
+    LINALG_FUNCTION typename matrix<_R, _C, T>::col_type operator/( matrix<_R, _C, T> const                    &m,
+                                                                    typename matrix<_R, _C, T>::row_type const &v )
     {
         return inverse( m ) * v;
     }
 
-    template <size_t _Rows, size_t _Columns, typename T>
-    LINALG_FUNCTION typename matrix<_Rows, _Columns, T>::row_type operator/( typename matrix<_Rows, _Columns, T>::col_type const &v,
-                                                                             matrix<_Rows, _Columns, T> const                    &m )
+    template <size_t _R, size_t _C, typename T>
+    LINALG_FUNCTION typename matrix<_R, _C, T>::row_type operator/( typename matrix<_R, _C, T>::col_type const &v,
+                                                                    matrix<_R, _C, T> const                    &m )
     {
         return v * inverse( m );
     }
 
-    template <size_t _Rows, size_t _Columns, typename T>
-    LINALG_FUNCTION bool operator==( matrix<_Rows, _Columns, T> const &m1, matrix<_Rows, _Columns, T> const &m2 )
+    template <size_t _R, size_t _C, typename T>
+    LINALG_FUNCTION bool operator==( matrix<_R, _C, T> const &m1, matrix<_R, _C, T> const &m2 )
     {
         bool result = true;
 
@@ -38,8 +38,8 @@ namespace numlua::linalg
         return result;
     }
 
-    template <size_t _Rows, size_t _Columns, typename T>
-    LINALG_FUNCTION bool operator!=( matrix<_Rows, _Columns, T> const &m1, matrix<_Rows, _Columns, T> const &m2 )
+    template <size_t _R, size_t _C, typename T>
+    LINALG_FUNCTION bool operator!=( matrix<_R, _C, T> const &m1, matrix<_R, _C, T> const &m2 )
     {
         bool result = false;
 
