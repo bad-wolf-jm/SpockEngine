cf8149777124e6318d3a89d7cfc8ed496df42334
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-29 15:37:13 -0400

    Fixed unit tests for texture module

--- Source/Core/CUDA/Texture/TextureData.cpp
+++ Source/Core/CUDA/Texture/TextureData.cpp
@@ -26,46 +26,6 @@
 
 namespace numlua::core
 {
-
-    image_data_t LoadImageData( fs::path const &aPath )
-    {
-        constexpr size_t lComponentCount = 4;
-
-        image_data_t lImageData{};
-        int32_t    lActualComponentCount = 0;
-        int32_t    lWidth                = 0;
-        int32_t    lHeight               = 0;
-        size_t     lChannelSize          = 0;
-
-        if( stbi_is_hdr( aPath.string().c_str() ) )
-        {
-            lChannelSize = 4;
-
-            float *lData = stbi_loadf( aPath.string().c_str(), &lWidth, &lHeight, &lActualComponentCount, lComponentCount );
-
-            if( !lData ) return image_data_t{};
-
-            lImageData.mFormat    = color_format::RGBA32_FLOAT;
-            lImageData.mPixelData = vector_t<uint8_t>( (uint8_t*)lData, ((uint8_t*)lData )+ ( lWidth * lHeight * sizeof( float ) ) );
-        }
-        else
-        {
-            lChannelSize = 1;
-
-            stbi_uc *lData = stbi_load( aPath.string().c_str(), &lWidth, &lHeight, &lActualComponentCount, lComponentCount );
-
-            if( !lData ) return image_data_t{};
-
-            lImageData.mFormat    = color_format::RGBA8_UNORM;
-            lImageData.mPixelData = vector_t<uint8_t>( lData, lData + ( lWidth * lHeight * sizeof( float ) ) );
-        }
-
-        lImageData.mWidth    = static_cast<size_t>( lWidth );
-        lImageData.mHeight   = static_cast<size_t>( lHeight );
-        lImageData.mByteSize = lImageData.mWidth * lImageData.mHeight * lComponentCount * lChannelSize;
-        return lImageData;
-    }
-
     static const format ToGliType( const color_format &aFormat )
     {
         switch( aFormat )
