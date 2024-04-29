cefae30ad3dc126a2f3ba80bdf897f61b1fb5c04
Jean-Martin Albert <jm.albert@gmail.com>
2024-04-29 15:38:07 -0400

    Code formatting

--- Source/Core/CUDA/Texture/TextureData.cpp
+++ Source/Core/CUDA/Texture/TextureData.cpp
@@ -20,9 +20,9 @@
 
 #include "Core/Logging.h"
 
+#include <Core/Textures/generate_mipmaps.hpp>
 #include <filesystem>
 #include <fstream>
-#include <Core/Textures/generate_mipmaps.hpp>
 
 namespace numlua::core
 {
@@ -30,26 +30,46 @@ namespace numlua::core
     {
         switch( aFormat )
         {
-        case color_format::R32_FLOAT: return FORMAT_R32_SFLOAT_PACK32;
-        case color_format::RG32_FLOAT: return FORMAT_RG32_SFLOAT_PACK32;
-        case color_format::RGB32_FLOAT: return FORMAT_RGB32_SFLOAT_PACK32;
-        case color_format::RGBA32_FLOAT: return FORMAT_RGBA32_SFLOAT_PACK32;
-        case color_format::R16_FLOAT: return FORMAT_R16_SFLOAT_PACK16;
-        case color_format::RG16_FLOAT: return FORMAT_RG16_SFLOAT_PACK16;
-        case color_format::RGB16_FLOAT: return FORMAT_RGB16_SFLOAT_PACK16;
-        case color_format::RGBA16_FLOAT: return FORMAT_RGBA16_SFLOAT_PACK16;
-        case color_format::R8_UNORM: return FORMAT_R8_UNORM_PACK8;
-        case color_format::RG8_UNORM: return FORMAT_RG8_UNORM_PACK8;
-        case color_format::RGB8_UNORM: return FORMAT_RGB8_UNORM_PACK8;
-        case color_format::RGBA8_UNORM: return FORMAT_RGBA8_UNORM_PACK8;
-        case color_format::D16_UNORM: return FORMAT_D16_UNORM_PACK16;
-        case color_format::X8_D24_UNORM_PACK32: return FORMAT_D24_UNORM_PACK32;
-        case color_format::D32_SFLOAT: return FORMAT_D32_SFLOAT_PACK32;
-        case color_format::S8_UINT: return FORMAT_R8_SNORM_PACK8;
-        case color_format::D16_UNORM_S8_UINT: return FORMAT_D16_UNORM_S8_UINT_PACK32;
-        case color_format::D24_UNORM_S8_UINT: return FORMAT_D24_UNORM_S8_UINT_PACK32;
-        case color_format::D32_UNORM_S8_UINT: return FORMAT_D32_SFLOAT_S8_UINT_PACK64;
-        default: return FORMAT_UNDEFINED;
+        case color_format::R32_FLOAT:
+            return FORMAT_R32_SFLOAT_PACK32;
+        case color_format::RG32_FLOAT:
+            return FORMAT_RG32_SFLOAT_PACK32;
+        case color_format::RGB32_FLOAT:
+            return FORMAT_RGB32_SFLOAT_PACK32;
+        case color_format::RGBA32_FLOAT:
+            return FORMAT_RGBA32_SFLOAT_PACK32;
+        case color_format::R16_FLOAT:
+            return FORMAT_R16_SFLOAT_PACK16;
+        case color_format::RG16_FLOAT:
+            return FORMAT_RG16_SFLOAT_PACK16;
+        case color_format::RGB16_FLOAT:
+            return FORMAT_RGB16_SFLOAT_PACK16;
+        case color_format::RGBA16_FLOAT:
+            return FORMAT_RGBA16_SFLOAT_PACK16;
+        case color_format::R8_UNORM:
+            return FORMAT_R8_UNORM_PACK8;
+        case color_format::RG8_UNORM:
+            return FORMAT_RG8_UNORM_PACK8;
+        case color_format::RGB8_UNORM:
+            return FORMAT_RGB8_UNORM_PACK8;
+        case color_format::RGBA8_UNORM:
+            return FORMAT_RGBA8_UNORM_PACK8;
+        case color_format::D16_UNORM:
+            return FORMAT_D16_UNORM_PACK16;
+        case color_format::X8_D24_UNORM_PACK32:
+            return FORMAT_D24_UNORM_PACK32;
+        case color_format::D32_SFLOAT:
+            return FORMAT_D32_SFLOAT_PACK32;
+        case color_format::S8_UINT:
+            return FORMAT_R8_SNORM_PACK8;
+        case color_format::D16_UNORM_S8_UINT:
+            return FORMAT_D16_UNORM_S8_UINT_PACK32;
+        case color_format::D24_UNORM_S8_UINT:
+            return FORMAT_D24_UNORM_S8_UINT_PACK32;
+        case color_format::D32_UNORM_S8_UINT:
+            return FORMAT_D32_SFLOAT_S8_UINT_PACK64;
+        default:
+            return FORMAT_UNDEFINED;
         }
     }
 
@@ -57,26 +77,46 @@ namespace numlua::core
     {
         switch( aFormat )
         {
-        case FORMAT_R32_SFLOAT_PACK32: return color_format::R32_FLOAT;
-        case FORMAT_RG32_SFLOAT_PACK32: return color_format::RG32_FLOAT;
-        case FORMAT_RGB32_SFLOAT_PACK32: return color_format::RGB32_FLOAT;
-        case FORMAT_RGBA32_SFLOAT_PACK32: return color_format::RGBA32_FLOAT;
-        case FORMAT_R16_SFLOAT_PACK16: return color_format::R16_FLOAT;
-        case FORMAT_RG16_SFLOAT_PACK16: return color_format::RG16_FLOAT;
-        case FORMAT_RGB16_SFLOAT_PACK16: return color_format::RGB16_FLOAT;
-        case FORMAT_RGBA16_SFLOAT_PACK16: return color_format::RGBA16_FLOAT;
-        case FORMAT_R8_UNORM_PACK8: return color_format::R8_UNORM;
-        case FORMAT_RG8_UNORM_PACK8: return color_format::RG8_UNORM;
-        case FORMAT_RGB8_UNORM_PACK8: return color_format::RGB8_UNORM;
-        case FORMAT_RGBA8_UNORM_PACK8: return color_format::RGBA8_UNORM;
-        case FORMAT_D16_UNORM_PACK16: return color_format::D16_UNORM;
-        case FORMAT_D24_UNORM_PACK32: return color_format::X8_D24_UNORM_PACK32;
-        case FORMAT_D32_SFLOAT_PACK32: return color_format::D32_SFLOAT;
-        case FORMAT_R8_SNORM_PACK8: return color_format::S8_UINT;
-        case FORMAT_D16_UNORM_S8_UINT_PACK32: return color_format::D16_UNORM_S8_UINT;
-        case FORMAT_D24_UNORM_S8_UINT_PACK32: return color_format::D24_UNORM_S8_UINT;
-        case FORMAT_D32_SFLOAT_S8_UINT_PACK64: return color_format::D32_UNORM_S8_UINT;
-        default: return color_format::UNDEFINED;
+        case FORMAT_R32_SFLOAT_PACK32:
+            return color_format::R32_FLOAT;
+        case FORMAT_RG32_SFLOAT_PACK32:
+            return color_format::RG32_FLOAT;
+        case FORMAT_RGB32_SFLOAT_PACK32:
+            return color_format::RGB32_FLOAT;
+        case FORMAT_RGBA32_SFLOAT_PACK32:
+            return color_format::RGBA32_FLOAT;
+        case FORMAT_R16_SFLOAT_PACK16:
+            return color_format::R16_FLOAT;
+        case FORMAT_RG16_SFLOAT_PACK16:
+            return color_format::RG16_FLOAT;
+        case FORMAT_RGB16_SFLOAT_PACK16:
+            return color_format::RGB16_FLOAT;
+        case FORMAT_RGBA16_SFLOAT_PACK16:
+            return color_format::RGBA16_FLOAT;
+        case FORMAT_R8_UNORM_PACK8:
+            return color_format::R8_UNORM;
+        case FORMAT_RG8_UNORM_PACK8:
+            return color_format::RG8_UNORM;
+        case FORMAT_RGB8_UNORM_PACK8:
+            return color_format::RGB8_UNORM;
+        case FORMAT_RGBA8_UNORM_PACK8:
+            return color_format::RGBA8_UNORM;
+        case FORMAT_D16_UNORM_PACK16:
+            return color_format::D16_UNORM;
+        case FORMAT_D24_UNORM_PACK32:
+            return color_format::X8_D24_UNORM_PACK32;
+        case FORMAT_D32_SFLOAT_PACK32:
+            return color_format::D32_SFLOAT;
+        case FORMAT_R8_SNORM_PACK8:
+            return color_format::S8_UINT;
+        case FORMAT_D16_UNORM_S8_UINT_PACK32:
+            return color_format::D16_UNORM_S8_UINT;
+        case FORMAT_D24_UNORM_S8_UINT_PACK32:
+            return color_format::D24_UNORM_S8_UINT;
+        case FORMAT_D32_SFLOAT_S8_UINT_PACK64:
+            return color_format::D32_UNORM_S8_UINT;
+        default:
+            return color_format::UNDEFINED;
         }
     }
 
@@ -84,9 +124,11 @@ namespace numlua::core
     {
         switch( aTextureType )
         {
-        case texture_type::TEXTURE_3D: return TARGET_3D;
+        case texture_type::TEXTURE_3D:
+            return TARGET_3D;
         case texture_type::TEXTURE_2D:
-        default: return TARGET_2D;
+        default:
+            return TARGET_2D;
         }
     }
 
@@ -94,9 +136,11 @@ namespace numlua::core
     {
         switch( aTextureType )
         {
-        case TARGET_3D: return texture_type::TEXTURE_3D;
+        case TARGET_3D:
+            return texture_type::TEXTURE_3D;
         case TARGET_2D:
-        default: return texture_type::TEXTURE_2D;
+        default:
+            return texture_type::TEXTURE_2D;
         }
     }
 
@@ -104,9 +148,11 @@ namespace numlua::core
     {
         switch( aTextureType )
         {
-        case sampler_filter::NEAREST: return FILTER_NEAREST;
+        case sampler_filter::NEAREST:
+            return FILTER_NEAREST;
         case sampler_filter::LINEAR:
-        default: return FILTER_LINEAR;
+        default:
+            return FILTER_LINEAR;
         }
     }
 
@@ -114,9 +160,11 @@ namespace numlua::core
     {
         switch( aTextureType )
         {
-        case sampler_mipmap::NEAREST: return FILTER_NEAREST;
+        case sampler_mipmap::NEAREST:
+            return FILTER_NEAREST;
         case sampler_mipmap::LINEAR:
-        default: return FILTER_LINEAR;
+        default:
+            return FILTER_LINEAR;
         }
     }
 
@@ -124,12 +172,17 @@ namespace numlua::core
     {
         switch( aTextureType )
         {
-        case sampler_wrapping::REPEAT: return WRAP_REPEAT;
-        case sampler_wrapping::MIRRORED_REPEAT: return WRAP_MIRROR_REPEAT;
-        case sampler_wrapping::CLAMP_TO_EDGE: return WRAP_CLAMP_TO_EDGE;
-        case sampler_wrapping::MIRROR_CLAMP_TO_BORDER: return WRAP_MIRROR_CLAMP_TO_BORDER;
+        case sampler_wrapping::REPEAT:
+            return WRAP_REPEAT;
+        case sampler_wrapping::MIRRORED_REPEAT:
+            return WRAP_MIRROR_REPEAT;
+        case sampler_wrapping::CLAMP_TO_EDGE:
+            return WRAP_CLAMP_TO_EDGE;
+        case sampler_wrapping::MIRROR_CLAMP_TO_BORDER:
+            return WRAP_MIRROR_CLAMP_TO_BORDER;
         case sampler_wrapping::CLAMP_TO_BORDER:
-        default: return WRAP_CLAMP_TO_BORDER;
+        default:
+            return WRAP_CLAMP_TO_BORDER;
         }
     }
 
@@ -231,7 +284,7 @@ namespace numlua::core
     void TextureData::Initialize()
     {
         mInternalTexture = texture( ToGliType( mSpec.mType ), ToGliType( mSpec.mFormat ),
-                                         extent3d{ mSpec.mWidth, mSpec.mHeight, mSpec.mDepth }, 1, 1, mSpec.mMipLevels );
+                                    extent3d{ mSpec.mWidth, mSpec.mHeight, mSpec.mDepth }, 1, 1, mSpec.mMipLevels );
     }
 
     TextureData2D::TextureData2D( texture_create_info_t const &aCreateInfo )
@@ -265,16 +318,17 @@ namespace numlua::core
 
     image_data_t TextureData2D::GetImageData()
     {
-        vector_t<uint8_t> lImageData( (uint8_t*)mInternalTexture2d.data(), ((uint8_t*)mInternalTexture2d.data()) + mInternalTexture2d.size() );
+        vector_t<uint8_t> lImageData( (uint8_t *)mInternalTexture2d.data(),
+                                      ( (uint8_t *)mInternalTexture2d.data() ) + mInternalTexture2d.size() );
         return { mSpec.mFormat, static_cast<size_t>( mSpec.mWidth ), static_cast<size_t>( mSpec.mHeight ), mInternalTexture2d.size(),
                  std::move( lImageData ) };
     }
 
     TextureSampler2D::TextureSampler2D( TextureData2D const &aTexture, texture_sampling_info_t const &aSamplingInfo )
-        : sampler2d<float>( aTexture.mInternalTexture2d, ToGliType( aSamplingInfo.mWrapping ),
-                                 ToGliType( aSamplingInfo.mMipFilter ), ToGliType( aSamplingInfo.mFilter ),
-                                 vec4{ aSamplingInfo.mBorderColor[0], aSamplingInfo.mBorderColor[1],
-                                            aSamplingInfo.mBorderColor[2], aSamplingInfo.mBorderColor[3] } )
+        : sampler2d<float>( aTexture.mInternalTexture2d, ToGliType( aSamplingInfo.mWrapping ), ToGliType( aSamplingInfo.mMipFilter ),
+                            ToGliType( aSamplingInfo.mFilter ),
+                            vec4{ aSamplingInfo.mBorderColor[0], aSamplingInfo.mBorderColor[1], aSamplingInfo.mBorderColor[2],
+                                  aSamplingInfo.mBorderColor[3] } )
         , mSamplingSpec{ aSamplingInfo }
     {
     }
@@ -304,7 +358,7 @@ namespace numlua::core
     }
 
     TextureDataCubeMap::TextureDataCubeMap( vector_t<uint8_t> aKTXData, uint32_t aSize )
-        : TextureData( (const char*)aKTXData.data(), aSize )
+        : TextureData( (const char *)aKTXData.data(), aSize )
     {
         mInternalTextureCubeMap = texture_cube( mInternalTexture );
     }
@@ -313,9 +367,7 @@ namespace numlua::core
     {
         sCubeMapImageData lImageData{};
 
-        return std::move(lImageData);
+        return std::move( lImageData );
     }
 
-
-
-} // namespace SE::Core
+} // namespace numlua::core
